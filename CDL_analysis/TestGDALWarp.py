a30715aba553040e95acf898cddddfce69323c00
LejoFlores <lejoflores@boisestate.edu>
2018-11-18 22:16:01 -0700

    refactoring

--- CDL_analysis/TestGDALWarp.py
+++ CDL_analysis/TestGDALWarp.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-GDAL_res = 0.25
+GDAL_res = 0.05
 
 GCAM_GridDir  = '/Users/lejoflores/IM3-BoiseState/CDL_analysis/GCAM_SRP/'
 GCAM_GridFile = 'gcam_2010_srb.tiff'
@@ -20,19 +20,41 @@ GCAM_GridFile = 'gcam_2010_srb.tiff'
 GCAM_AggWriteDir  = GCAM_GridDir+'3km/'
 GCAM_AggWriteFile = 'test_3km.tiff'
 
-ds = gdal.Open(GCAM_GridDir+GCAM_GridFile)
+src_ds = gdal.Open(GCAM_GridDir+GCAM_GridFile)
 
-gcam_projection = ds.GetProjection()
+src_geot = src_ds.GetGeoTransform()
+src_proj = src_ds.GetProjection()
+src_res  = src_ds.GetGeoTransform()[1]
+
+agg_factor = GDAL_res / src_res
+
+src_ncols = src_ds.RasterXSize
+src_nrows = src_ds.RasterYSize
+
+dst_ncols = (int)(src_ncols/agg_factor)
+dst_nrows = (int)(src_nrows/agg_factor)
 
 
-warping = gdal.WarpOptions(format='Gtiff', xRes=GDAL_res, yRes=GDAL_res, srcSRS=gcam_projection, resampleAlg='mode')
-gdal.Warp(GCAM_AggWriteDir+GCAM_AggWriteFile, ds, warpOptions=warping)
+dst_driver = gdal.GetDriverByName('Gtiff')
 
-ds = None
+dst_geot = (src_geot[0], src_geot[1]*agg_factor, src_geot[2], src_geot[3], src_geot[4], src_geot[5]*agg_factor)
+dst_proj = src_proj
 
-ds = gdal.Open(GCAM_AggWriteDir+GCAM_AggWriteFile)
+dst_ds = dst_driver.Create(GCAM_AggWriteDir+GCAM_AggWriteFile, dst_ncols, dst_nrows, 1, gdal.GDT_Float32)
+dst_ds.SetGeoTransform(dst_geot)
+dst_ds.SetProjection(dst_proj)
 
+warping = gdal.WarpOptions(format='Gtiff', xRes=GDAL_res, yRes=GDAL_res, srcSRS=dst_proj, resampleAlg=gdal.GRA_Mode)
+
+#gdal.Warp(GCAM_AggWriteDir+GCAM_AggWriteFile, src_ds, warpOptions=warping)
+gdal.ReprojectImage(src_ds, dst_ds, src_proj, dst_proj, gdal.GRA_Mode)
+
+
+src_ds = None
+#
+
+ds   = gdal.Open(GCAM_AggWriteDir+GCAM_AggWriteFile)
 grid = ds.ReadAsArray()
 
-plt.figure(figsize=(12,10),dpi=300)
+plt.figure(dpi=100)
 plt.imshow(grid)
