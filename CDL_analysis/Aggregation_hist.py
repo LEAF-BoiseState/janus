e93891edecd46b689325f15bcf1a457cdd179d00
kendrakaiser <kendra.kaiser@gmail.com>
2019-01-07 16:08:59 -0700

    Added new tools

--- CDL_analysis/Aggregation_hist.py
+++ CDL_analysis/Aggregation_hist.py
@@ -10,6 +10,8 @@ import ogr
 import matplotlib.pyplot as plt
 import shapefile
 import numpy as np
+import rasterio
+from rasterstats import zonal_stats #zonal stats http://www.perrygeo.com/index2.html, https://pythonhosted.org/rasterstats/manual.html
 #
 
 
@@ -20,19 +22,24 @@ import numpy as np
 #histogram from polygon (zonal/focal histogram)
 
 #sample code http://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html#polygonize-a-raster-band
+
 #=============================================================================#
 # Set working  directories                                                    #
 #=============================================================================#
 
 GCAM_ReadDir = '/Users/kendrakaiser/Documents/Data/GCAM_UTM'
 GCAM_ReadFile_1km = '/1km/gcam_2010_srb_1000_utm11N.tiff'
+
+
+
 GCAM_ReadFile_3km = '/3km/gcam_2010_srb_utm11N_3000_utm11N.tiff'
+src_ds3km = gdal.Open(GCAM_ReadDir+GCAM_ReadFile_3km)
+
 
 src_ds1km = gdal.Open(GCAM_ReadDir+GCAM_ReadFile_1km)
-
 srcband = src_ds1km.GetRasterBand(1)
 
-src_ds3km = gdal.Open(GCAM_ReadDir+GCAM_ReadFile_3km)
+
 
 #=============================================================================#
 #Create Polygon from grid                                                     #
@@ -43,8 +50,11 @@ drv = ogr.GetDriverByName("ESRI Shapefile")
 dst_ds = drv.CreateDataSource( dst_layername + ".shp" )
 dst_layer = dst_ds.CreateLayer(dst_layername, srs = 'EPSG:32611' )
 
+#creates vector polygons for all connected regions of pixels in the raster sharing a common pixel value- not quite what we need
 tst= gdal.Polygonize(srcband, None, dst_layer, -1, [], callback=None )
 
 tst= np.float64(srcband.ReadAsArray())
 #sf = shapefile.Reader("SRB_counties")
 plt.imshow(tst)
+plt.grid(True)
+
