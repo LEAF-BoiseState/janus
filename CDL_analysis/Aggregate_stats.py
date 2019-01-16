22803add169230155279dacf6e42ba2f515a4d09
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-01-16 15:07:52 -0700

    Added notes for next steps

--- CDL_analysis/Aggregate_stats.py
+++ CDL_analysis/Aggregate_stats.py
@@ -7,12 +7,15 @@ Created on Wed Jan 16 09:41:43 2019
 """
 from rasterstats import zonal_stats #zonal stats http://www.perrygeo.com/index2.html, https://pythonhosted.org/rasterstats/manual.html
 
-#looping through polygon
-#cal histogram for one polygon
-#histogram from polygon (zonal/focal histogram)
+#create histogram of each year at each scale
+#ts plots of each land cover at each scale
+#loop through polygon
+#use 1km polygon to get histograms of 30m data --> how to summarize this though?
+
 
 #sample code http://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html#polygonize-a-raster-band
-
+# https://rasterio.readthedocs.io/en/latest/
+# https://pythonhosted.org/rasterstats/manual.html
 
 ReadDir='~/Documents/GitRepos/IM3-BoiseState/CDL_analysis/'
 shpFile='SRB_poly_3km.shp'
