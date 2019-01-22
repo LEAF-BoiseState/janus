8cd8f69cd2dfc85f43896650d6957b27554c442c
LejoFlores <lejoflores@boisestate.edu>
2019-01-21 23:09:43 -0700

    added an outline for ABM algorithm

--- ABMdev/LULCC_ABM_v0.py
+++ ABMdev/LULCC_ABM_v0.py
@@ -1,1 +1,35 @@
 # Outline of algorithm...
+
+# 0. Declare some key variables
+#   - Name of spatial domain map with LULCC classes 
+#   - Name of spatial domain map with active/inactive cells (e.g., national forests, etc.)
+#   - Name of spatial domain map of urban expansion areas
+#   - Name of spatial domain map of any ag attributes needed (e.g., type of ag - leased etc.)
+#   - Number of simulation years or time steps
+#   - Output interval
+#   - Output base name
+
+# 1. Preprocessing
+#   - Load spatial maps
+#   - Set up any agent classes for spatial domains
+#   - Create any temporary, derived spatial domains
+#   - Create storage containers for LULCC simulations (NaNs outside active domain?)
+#   - Create any exogenous agent types needed (e.g., regulators, global markets)
+
+# 2. Simulation loop
+#   - Loop through time
+#       > Do any needed statistics at the beginning of time (i.e., update neighborhood stats for farmers, urban areas, etc.)
+#       > Get any new global info needed (i.e., value of crops, number of potential developers in reservoir)
+#       > Get all farmer types
+#           - Compute happiness metric based on urbanness, global crop price
+#           - Unhappy agents:
+#               > If in urban expansion zone, choose between: stay, sell, switch to corporate
+#               > If not in urban expansion zone, choose between: stay, sell (exurb), switch to corporate 
+#       > Decrement urban types depending on number of farmers that sell
+#       > Decrement exurban types developing on number of farmers that sell
+#       > Urban fraction inside development zone > threshold?
+#           - Yes: expand urban zone to accommodate 20 years' growth (this actually comes from COMPASS)
+#           - No: Do nothing
+#       > Add new urban types for next year
+#       > Add new exurban types for next year
+
