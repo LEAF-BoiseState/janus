4a04217254cdb849a9016070225da0fd9f3a6f2e
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-01-23 16:45:54 -0700

    Individual Clip is working

--- CDL_analysis/SRB2counties.py
+++ CDL_analysis/SRB2counties.py
@@ -7,47 +7,32 @@ Created on Sun Nov  4 17:54:39 2018
 """
 import os
 import glob
-
+import geopandas as gpd
 import rasterio as rio
 from rasterio.mask import mask
-from shapely.geometry import mapping
-import matplotlib.pyplot as plt
-import geopandas as gpd
-
-
-import numpy as np
 from rasterio.plot import show
-
+from shapely.geometry import mapping
 
 os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/CDL_analysis/Shapefiles')
 
-Ada = gpd.read_file('Ada.shp')
+Ada = gpd.read_file('Ada.shp') #-->GeoPandas dataframe
+Ada=Ada.to_crs(epsg=32611) #reproject bc for some reason not saving the crs from Arc
 
 os.chdir('/Users/kendrakaiser/Documents/Data/GCAM_UTM/1km')
 files = glob.glob('*.tiff')
 
-# open data
+
 with rio.open(files[0]) as src:
-    cdl = src.read(masked = True)[0]
-    extent = rio.plot.plotting_extent(src)
-    cdl_profile = src.profile
-
-
-
-
-
-fig, ax = plt.subplots(figsize = (10,10))
-ax.imshow(cdl, 
-          cmap='terrain', 
-          extent=extent)
-Ada.plot(ax=ax, alpha=.6, color='g');
-
-
-
-with rio.open(Ada) as src:
     extent_geojson = mapping(Ada['geometry'][0])
-    lidar_chm_crop, crop_affine = mask(src, 
-                                   shapes=[extent_geojson], 
-                                   crop=True)
+    Ada_cdl, affine = mask(src, 
+                            [extent_geojson], 
+                            crop=True)
     # metadata for writing or exporting the data
-    cdl_meta = src.meta.copy()
+    Ada_meta = src.meta.copy()
+
+    show(Ada_cdl)
+    
+# Write data
+writeFile = "data/colorado-flood/spatial/outputs/lidar_chm_cropped.tif"
+with rio.open(writeFile, 'w', **Ada_meta) as ff:
+    ff.write(Ada_cdl[0], 1)
