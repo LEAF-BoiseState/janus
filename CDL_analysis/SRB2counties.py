83cf186a981993bca1d2e1cc924b83f905657c44
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-01-23 15:15:32 -0700

    Added shapefiles of Ada and Jerome counties

--- CDL_analysis/SRB2counties.py
+++ CDL_analysis/SRB2counties.py
@@ -5,28 +5,34 @@ Created on Sun Nov  4 17:54:39 2018
 
 @author: kek25
 """
+import os
+import glob
 
 import rasterio as rio
-from rasterio.plot import show
 from rasterio.mask import mask
 from shapely.geometry import mapping
-import numpy as np
-import os
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-os.chdir("D:/Dropbox/BSU/Python/Data/CDL")
-#os.chdir("/Users/kek25/Dropbox/BSU/Python/Data/CDL")
-files = glob.glob('*.txt')
 
-# open the lidar chm
+import numpy as np
+from rasterio.plot import show
+
+
+os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/CDL_analysis/Shapefiles')
+
+Ada = gpd.read_file('Ada.shp')
+
+os.chdir('/Users/kendrakaiser/Documents/Data/GCAM_UTM/1km')
+files = glob.glob('*.tiff')
+
+# open data
 with rio.open(files[0]) as src:
     cdl = src.read(masked = True)[0]
     extent = rio.plot.plotting_extent(src)
     cdl_profile = src.profile
 
 
-counties = gpd.read_file('SRB_counties.shp')
 
 
 
@@ -34,12 +40,12 @@ fig, ax = plt.subplots(figsize = (10,10))
 ax.imshow(cdl, 
           cmap='terrain', 
           extent=extent)
-counties.plot(ax=ax, alpha=.6, color='g');
+Ada.plot(ax=ax, alpha=.6, color='g');
 
 
 
-with rio.open(counties) as src:
-    extent_geojson = mapping(counties['geometry'][0])
+with rio.open(Ada) as src:
+    extent_geojson = mapping(Ada['geometry'][0])
     lidar_chm_crop, crop_affine = mask(src, 
                                    shapes=[extent_geojson], 
                                    crop=True)
