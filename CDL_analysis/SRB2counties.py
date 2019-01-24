2fac21b18fb4b685c17a700ea872e56b2f358ade
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-01-23 17:58:12 -0700

    Loop through files working

--- CDL_analysis/SRB2counties.py
+++ CDL_analysis/SRB2counties.py
@@ -4,6 +4,10 @@
 Created on Sun Nov  4 17:54:39 2018
 
 @author: kek25
+
+Subsets data by county
+
+Future iteration could subset each county in the SRB
 """
 import os
 import glob
@@ -12,33 +16,35 @@ import rasterio as rio
 from rasterio.mask import mask
 from rasterio.plot import show
 from shapely.geometry import mapping
+import numpy as np
 
 os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/CDL_analysis/Shapefiles')
 
-Ada = gpd.read_file('Ada.shp') #-->GeoPandas dataframe
-Ada=Ada.to_crs(epsg=32611) #reproject bc for some reason not saving the crs from Arc
+county = gpd.read_file('Jerome.shp') #-->GeoPandas dataframe
+county=county.to_crs(epsg=32611) #reproject bc for some reason not saving the crs from Arc
 
-os.chdir('/Users/kendrakaiser/Documents/Data/GCAM_UTM/1km')
+os.chdir('/Users/kendrakaiser/Documents/Data/GCAM_UTM/30m')
 files = glob.glob('*.tiff')
 
-
-with rio.open(files[0]) as src:
-    extent_geojson = mapping(Ada['geometry'][0])
-    Ada_cdl, affine = mask(src, 
+for i in np.arange(8):
+    with rio.open(files[i]) as src:
+        extent_geojson = mapping(county['geometry'][0])
+        county_cdl, affine = mask(src, 
                             [extent_geojson], 
                             crop=True)
-    # metadata for writing or exporting the data
-    Ada_meta = src.meta.copy()
-    Ada_meta.update({"driver": "GTiff",
-                     "height": Ada_cdl.shape[1],
-                     "width": Ada_cdl.shape[2],
-                     "transform": affine,
-                     "crs":32611})
-
-    show(Ada_cdl)
+        # metadata for writing or exporting the data
+        county_meta = src.meta.copy()
+        county_meta.update({"height": county_cdl.shape[1],
+                         "width": county_cdl.shape[2],
+                         "transform": affine})
     
-# Write data
-writeDir='/Users/kendrakaiser/Documents/Data/GCAM_UTM/1km/Ada/'
-writeFile = files[0].replace('srb','Ada')
-with rio.open(writeDir+writeFile, 'w', **Ada_meta) as out:
-    out.write(Ada_cdl)
+        # Write data
+        writeDir='/Users/kendrakaiser/Documents/Data/GCAM_UTM/30m/Jerome/'
+        writeFile = files[i].replace('srb','Jerome')
+        with rio.open(writeDir+writeFile, 'w', **county_meta) as out:
+            out.write(county_cdl)
+    
+    # Clean up
+    src = None
+            
+show(county_cdl)
