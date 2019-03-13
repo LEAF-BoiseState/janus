fece48f1bba93707214a632f2b379de8aea5eb2b
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-03-13 10:10:40 -0600

    Cleaning up, trying multiple counties

--- ABMdev/Calc_Min_Distance.py
+++ ABMdev/Calc_Min_Distance.py
@@ -5,65 +5,39 @@ Created on Thu Mar  7 10:51:14 2019
 Calculate distance to closest cell of a given land cover
 @author: kek25
 """
-import numpy as np
-import math
-
-samp=np.random.randint(low = 2, size=(5,6))
-samp = np.array([[0,0,1,1,0,0],[0,0,0,1,0,0], [0,0,0,1,1,1], [0,0,0,0,1,0], [0,0,0,0,0,0]])
-
-city = np.array(np.nonzero(samp))
-other = np.array(np.where(samp==0))
-
-#x=other[1,:]
-#y=other[0,:]
-
-d_temp = np.empty(city.shape[1]) #faster than zeros, but b careful!
-minDist= np.empty(other.shape[1])
-dist=np.zeros(samp.shape)
-#turn into function
-for i in np.arange(other.shape[1]):
-  #go through each non-city cell
-   x=other[1,i]
-   y=other[0,i]
-   
-   #calculate distance to each of the city cells 
-   for j in np.arange(city.shape[1]):
-       xc=city[1,j]
-       yc=city[0,j]
-       
-       if ((x == xc + 1 or x == xc -1 or x == xc) & (y == yc + 1 or y == yc -1 or y == yc)):
-           d_temp[j] = 0
-       elif (x == xc or y == yc):
-           d_temp[j] = (abs(x-xc) + abs(y-yc)) - 1
-       else: 
-           d_temp[j] = math.sqrt((abs(x-xc)-1)**2 + (abs(y-yc)-1)**2)
-           
-       minDist[i] = min(d_temp)
-
-   dist[y,x]=minDist[i]    
-   
-##########
-# Import ada county 
-####
     
 import os  
 import geopandas as gp
 import numpy as np
 
-os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/')
-#os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/')
+#set user directory
+#os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/')
+os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/')
 
 SRB_3km= gp.read_file('CDL_analysis/Shapefiles/SRB_gridpolys/SRB_poly_3km_V2.shp')
 counties_shp= gp.read_file('CDL_analysis/Shapefiles/County_polys/Counties_SRB_clip_SingleID.shp')
 cities = gp.read_file('ABMdev/citylimits_Ada/citylimits.shp')
-cities=cities.to_crs(counties_shp.crs)
+cities=cities.to_crs(counties_shp.crs) #convert projection
 
-
-#select unique COUNTIES from SRB3km_poly 
+#select unique COUNTIES from SRB3km_poly * change the code here when we have all counties city info
 Ada_3km=SRB_3km[SRB_3km.geometry.intersects(counties_shp['geometry'][17])]
+Canyon_3km=SRB_3km[SRB_3km.geometry.intersects(counties_shp['geometry'][12])]
+Ada_Canyon=SRB_3km.loc[[12,17],:]#this doesnt work for plotting the two??
 
 Ada_3km.plot()
+Canyon_3km.plot()
 cities.plot()
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize = (5,5))
+Ada_3km.plot(ax=ax, 
+             color = 'green')
+Canyon_3km.plot(ax=ax,
+                color ='blue')
+#PROBLIMATIC - OVERLAPPING EDGES OF COUNTIES
+
+
 #join the cities data with the clipped ada county polygons
 Ada_cities_3km=gp.sjoin(Ada_3km, cities[['CITY', 'geometry']], how = 'left', op='intersects')
 #replace Nas
@@ -78,7 +52,10 @@ Ada_rural=Ada_cities_3km[Ada_cities_3km['CITY'] == 'Rural']
 Ada_cities=Ada_cities_3km[Ada_cities_3km['CITY'] != 'Rural']
 #calculate distance to closest city
 Ada_rural['distCity'] = Ada_rural.geometry.apply(lambda g: Ada_cities.distance(g).min())
-Ada_rural.plot(column='distCity', legend = True, figsize=(5,10))
+
+fig, ax = plt.subplots(figsize = (5,10))
+Ada_rural.plot(column='distCity', legend = True, figsize=(5,10), ax=ax)
+Ada_cities.plot(color='orange', ax=ax)
 
 
 ##RECOMBINING THEM DOESNT WORK YET
