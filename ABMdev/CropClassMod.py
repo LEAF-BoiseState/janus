9814088db6b6ac5b4fc1c2d653c0a542a7c3077c
LejoFlores <lejoflores@boisestate.edu>
2019-03-15 18:43:11 -0600

    refactoring

--- ABMdev/CropClassMod.py
+++ ABMdev/CropClassMod.py
@@ -6,3 +6,24 @@ Created on Fri Mar 15 18:33:36 2019
 @author: lejoflores
 """
 
+class Crop:
+    def __init__(self, CropID, Name="none", PreHarvestEnergyCosts, 
+        PreHarvestLaborCosts, PreHarvestMaterialCosts, HarvestEnergyCosts, 
+        HarvestLaborCosts, HarvestMaterialCosts, CropPrice, AreaCultivated):
+        
+        self.CropID = CropID
+        self.CropYield = CropYield
+        self.CropPrice = CropPrice
+
+        self.PreHarvestEnergyCosts = PreHarvestEnergyCosts
+        self.PreHarvestLaborCosts = PreHarvestLaborCosts
+        self.PreHarvestMaterialCosts = PreHarvestMaterialCosts
+        self.PreHarvestCosts = PreHarvestEnergyCosts + PreHarvestLaborCosts + PreHarvestMaterialCosts
+        
+        self.HarvestEnergyCosts = HarvestEnergyCosts
+        self.HarvestLaborCosts = HarvestLaborCosts
+        self.HarvestMaterialCosts = HarvestMaterialCosts
+        self.HarvestCosts = HarvestEnergyCosts + HarvestLaborCosts + HarvestMaterialCosts
+        
+        self.Costs = self.PreHarvestCosts + self.HarvestCosts # Cost per unit area of this crop
+        self.TotalCosts = self.AreaCultivated * self.Costs
