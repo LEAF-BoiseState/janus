b8b23668334cf2146832e6d15ec6824d97df87b5
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-03-27 07:28:17 -0600

    Cleaned Up

--- CDL_analysis/zonalStats_scales.py
+++ CDL_analysis/zonalStats_scales.py
@@ -23,26 +23,33 @@ import matplotlib.pyplot as plt
 from osgeo import gdal
 from math import log as ln
 
-#------- IMPORT DATA
+#=============================================================================#
+# Set working  directories and inout files                                    #
+#=============================================================================#
+
 os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/CDL_analysis')
 
-SRB_3km=gpd.read_file('Shapefiles/SRB_gridpolys/SRB_poly_3km_V2.shp')
-SRB_1km=gpd.read_file('Shapefiles/SRB_gridpolys/SRB_poly_3km_V2.shp')
+SRB_poly_3km=gpd.read_file('Shapefiles/SRB_gridpolys/SRB_poly_3km_V2.shp')
+SRB_poly_1km=gpd.read_file('Shapefiles/SRB_gridpolys/SRB_poly_1km_V2.shp')
 
 ReadDir = '/Users/kek25/Dropbox/BSU/IM3/Data/GCAM_UTM/30m/'
 files = glob.glob(ReadDir +'*.tiff')
 files.sort()
 
-#------- COPY RASTER for saving output from zonal stats
+#=============================================================================#
+# COPY raster for saving output from zonal stats- is shaefile better??                               
+#=============================================================================#
+ 
 g30 = gdal.Open(files[0])
 g30_meta = g30.meta.copy()
 gcam30m = np.array(g30.GetRasterBand(1).ReadAsArray())
 gcam30m[gcam30m==0] = np.nan #retains shape
 
-#------- Define shannons diversity index 
+#=============================================================================#
+# Define shannons diversity index                                
+#=============================================================================#
 def sdi(data):
-    """ Given a dictionary { 'species': count } , returns the SDI
-    >>> sdi({'a': 10, 'b': 20, 'c': 30,}) 1.01"""   
+    """ Given a dictionary { 'species': count }, returns sdi"""   
     def p(n, N):
         """ Relative abundance """
         if n is  0:
@@ -52,20 +59,31 @@ def sdi(data):
     N = sum(data.values())
     return -sum(p(n, N) for n in data.values() if n is not 0)
 
-#------- Retreive categorical counts for each pixel
-counts = zonal_stats(SRB_3km, files[0], categorical=True, geojson_out=True) # when no geojson_out it's just a list of dictionaries
-#------- define CDL keys for subsetting dictionary
-crop_keys=np.arange(1,28)
-# ------ Calculate and store Shannons Diversity Index
+#=============================================================================#
+# Retreive categorical counts for each pixel, calculate and store SDI                            
+#=============================================================================#
+# define CDL keys for subsetting dictionary
+crop_keys=np.arange(1,28) 
 
-sdix=[]
-for n in np.arange(len(counts)):
-    countx=counts[n]
-    count_prop=countx['properties'] 
-    cdls={k:count_prop[k] for k in crop_keys if k in count_prop} #subest data to dictionary of keys: count
-    sd=sdi(cdls)
-    sdix.append(sd)
-    counts[n].update({'sd' : sd})  #calculate Shannons Index append to dictionary
+def zonalSDI(SRB_poly, file):
     
-SRB_3km['sd']= sdix
-SRB_3km.plot(column='sd') #WORKS! need to mask out the nans
+    counts = zonal_stats(SRB_poly, file, categorical=True, geojson_out=True) # when no geojson_out it's just a list of dictionaries
+
+    sdix=[]
+    for n in np.arange(len(counts)):
+        countx=counts[n]
+        count_prop=countx['properties'] 
+        #subest data to dictionary of keys: count
+        cdls={k:count_prop[k] for k in crop_keys if k in count_prop} 
+        sd=sdi(cdls) #calculate SDI
+        sdix.append(sd) #append to vector
+     
+    varName='sdi_30m_'+ os.path.basename(file)[5:9]
+    SRB_poly[varName]= sdix #append to the original polygons- create new name based on filename
+    
+
+#=============================================================================#
+#Save Output                                                                  #
+#=============================================================================#
+#SRB_poly.plot(column=varName) #WORKS! need to mask out the nans
+SRB_poly.to_file(filename=SRB_poly_3km_sri, driver="ESRI Shapefile")
