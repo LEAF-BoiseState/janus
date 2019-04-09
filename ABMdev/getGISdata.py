0eb1a91e3472c48f524da2e75510a48468e531c5
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-04-09 10:17:28 -0600

    Moved

---
+++ ABMdev/getGISdata.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 21:55:00 2019
+
+@author: kek25
+
+function that clips GIS data based on counties, year, and resolution
+"""
+
+def getGISdata(countyList, year, scale):
+    #urban extent
+    #area of influence
+    #distance to city
+    #no-build mask
+    #cdl data
+    
+    
+    
