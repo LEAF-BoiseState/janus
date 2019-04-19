641ad864e2a2abd74f34296c59fcbd240c118a92
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-04-19 10:57:10 -0600

    commit

--- ABMdev/getGISdata.py
+++ ABMdev/getGISdata.py
@@ -7,18 +7,47 @@ Created on Mon Apr  8 21:55:00 2019
 
 function that clips GIS data based on counties, year, and resolution
 """
+import os  
+import geopandas as gp
 
-def getGISdata(countyList, year, scale):
-    #urban extent
+#set user directory
+os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/GIS_anlaysis/')
+#os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/GIS_anlaysis/Shapefiles/')
+DataPath= '/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/GIS_anlaysis/Shapefiles/'
+GCAMpath='/Users/kek25/Dropbox/BSU/Python/IM3/GCAM_SRP/'
+
+counties_shp= gp.read_file('Shapefiles/County_polys/Counties_SRB_clip_SingleID.shp')
+counties_shp=counties_shp.set_index('county')
+
+
+def getGISextent(countyList, scale):
+    
+    if scale == '3km':
+        SRB_poly= gp.read_file(DataPath+'SRB_gridpolys/SRB_poly_3km_V2.shp') 
+    elif scale == '1km':
+        SRB_poly= gp.read_file(DataPath+'SRB_gridpolys/SRB_poly_1km_V2.shp') 
+    
+    #select two shapefiles, this returns geometry of the union - this no longer distinguishes two
+    extent=counties_shp['geometry'].loc[countyList].unary_union #this is the row index, not the "COUNTY_ALL" index
+    extent_poly=SRB_poly[SRB_poly.geometry.intersects(extent)]
+    
+    extent_poly.plot()
+    return(extent_poly)
+    #urban extent, and distance to city - done
     #area of influence
-    #distance to city
     #no-build mask
     #cdl data
-    
-    
-    counties_shp= gp.read_file('County_polys/Counties_SRB_clip_SingleID.shp')
-    #select two shapefiles, this returns geometry of the union - this no longer distinguishes two
-    AC=counties_shp['geometry'][[12,17]].unary_union #this is the row index, not the "COUNTY_ALL" index
-    Ada_Canyon=SRB_3km[SRB_3km.geometry.intersects(AC)]
+
+
+def getGCAM(extent, year, scale):
+    #use scale to dtermine which folder to id
+    file=GCAMpath+scale+'/gcam_'+year
+
+
+
+
+countyList=['Ada', 'Canyon']  
+   
+extent=getGISdata(countyList, '3km')
     #also this probably just needs to be a raster ... 
-    Ada_Canyon.plot()
+    
