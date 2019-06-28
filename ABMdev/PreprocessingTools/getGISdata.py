d4324b208cbae3dbba4f40094ae68840478dcae9
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-06-27 18:07:38 -0600

    Still trying to get rasterize to work, added notes of things I tested

--- ABMdev/PreprocessingTools/getGISdata.py
+++ ABMdev/PreprocessingTools/getGISdata.py
@@ -67,20 +67,35 @@ def getGCAM(countyList, year, scale): #returns a numpy array
 #tryin to figureout hoew to turn the shapefile with two counties into a raster   
 def countyID(countyList, lc):
     
-    extent_shp=counties_shp['geometry'].loc[countyList]#figuerout how to save the value that can go into the raster
+    extent_shp=counties_shp['geometry'].loc[countyList]#this is a geoseries, figuerout how to save the value that can go into the raster
     numCounties=len(extent_shp)
-    es=gp.GeoDataFrame(extent_shp)
-    es=es.assign(value=np.arange(numCounties)) #or figure out how to make up the values
+    es=gp.GeoDataFrame(extent_shp) #this is a geodataframe
+    es=es.assign(value=np.arange(numCounties)) #add values to each shape
+
+#### none of this works
     coords = [json.loads(extent_shp.to_json())['features'][0]['geometry']] 
-    
     if numCounties > 1:
         for i in np.arange(1, numCounties):
             coords.append(json.loads(extent_shp.to_json())['features'][i]['geometry'])
-  
-    #this version dont work
+            
+        #this version dont work
    # coords =[json.loads(extent_shp.to_json())['features'][geom['geometry'] for geom in extent_shp] #parses features from GeoDataFrame the way rasterio wants them
     #shapes=features.shapes(extent_shp) input to this has to be a rasterio object
-    out=features.rasterize(coords, lc[0].shape, fill=999, all_touched=False)
+   # coords[0]['coordinates'] - this is a list of 2 lists that have explicit coordinates of locations (pixels??)
+    shapes=features.shapes(coords[0]['coordinates']) #'list' object has no attribute 'dtype'
+    shapes=((geometry,value) for geometry, value in SOMETHING) #this creates a generator which is an iterable
+    
+    outshape=(2809, 2838) # in the function it would be lc[0].shape
+    out=features.rasterize(shapes, outshape, fill=999, all_touched=False)
+    
+# this creates generator of geom, value pairs to use in rasterizing
+    shapes=((geometry,value) for geometry, value in es) #this creates a generator which is an iterable
+    #(es['geometry'], 0) this results in a tuple - geoseries, value pair, Invalid geometry object at index 0
+    #(es['geometry'][0], 0) this results in a tuple - polygon, value pair, int is not iterable
+    out = features.rasterize(shapes=es, out_shape=outshape, fill=999, all_touched=False)
+
+  
+
     
     return(out) 
 #------------------------------------------------------------------------
@@ -97,3 +112,7 @@ gcam_init=getGCAM(countyList, year, scale)
 
 extent_poly.to_file(DataPath+'ABMdev/Data/extent_1km_AdaCanyon.shp')
 np.save(DataPath+'ABMdev/Data/gcam_1km_2010_AdaCanyon.npy', gcam_init) #not sure if this one needs to be changed ...
+
+
+
+
