418449b2c685591cf026e56e39dcdf3641690d1f
Vicken Hillis <vickenhillis@boisestate.edu>
2019-07-02 14:13:18 -0600

    create social network

--- ABMdev/UrbanizationConformity.py
+++ ABMdev/UrbanizationConformity.py
@@ -2,45 +2,90 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 30 13:38:25 2019
-
 @author: Vicken Hillis
-
 Urbanization model incorporating a spatial prisoner's dilemma and conformist transmission of land ethic
 """
 
 import numpy as np
 import random
-import pandas as pd
 
-x = np.arange(1, 50)
-y = np.arange(1, 50)
+##### create a network of farmers
+##### farmers start in a two-dimensional matrix of size dim
+##### farmers take a random walk in cardinal dimensionsw
+##### any farmers that end a turn on the same cell become linked
 
-numstep = 50
-numcell = 2500
+#number of cells in each dimension in the landscape
+dim = 50
 
-import itertools
-def expandgrid(*itrs):
-   product = list(itertools.product(*itrs))
-   return pd.DataFrame.from_records(product, columns=['x','y'])
+# number of iterations in the random walk
+num_steps = 50
 
-product = list(itertools.product(x,y))
-pop = expandgrid(x,y)
-pop = np.meshgrid(x,y) # x is 0 and y is 1
+# number of agents in the network
+num_agents = dim^2
 
-adj_mat = np.zeros((numcell, numcell))
+# create a matrix of x,y locations for each individual
+pop = np.array([(x, y) for x in range(dim) for y in range(dim)])
 
+# create a network of zeros representing the social network
+adj_mat = np.zeros((2500,2500), dtype=int)
 
-#create base network connections between individuals
-
-#have agents move at random 
-for i in np.arange(numstep):
-    for j in np.arange(numcell):
+# number of iterations that the random walk will last
+for i in range(numstep):
+    
+    # loop through each of the nodes in the network
+    for j in range(numcell):
+        
+        # pick a random number between 1 and 4 representing the cardinal direction the agent will move
         z = random.randint(1,4)
         
+        # for each of the four cardinal directions, reassign the agent's location appropriately
         if z == 1:
-            if : 
-        elif :
             
-#create adjacency matrix by finding where agents overlap
+            pop[j,0] = pop[j,0] - 1
+            
+            # if the agent has left the dim X dim grid, reassign them to the other side            
+            if pop[j,0] < 0: pop[j,0] = dim - 1
+                
+        elif z == 2:
+            
+            pop[j,1] = pop[j,1] - 1
+            
+            if pop[j,1] < 0: pop[j,1] = dim - 1
+            
+        elif z == 3:
+            
+            pop[j,0] = pop[j,0] + 1
+            
+            if pop[j,0] == dim: pop[j,0] = 0
+            
+        elif z == 4:
+            
+            pop[j,1] = pop[j,1] + 1
+            
+            if pop[j,1] == dim: pop[j,1] = 0
+    
+    # after each of the dim^2 agents has moved, find all the agents in the same location
+    # for each row in pop array, find all other matching rows
+    for k in range(numcell):
+                
+        zzz = np.flatnonzero((pop == pop[k]).all(axis=1))
 
-# here is a new comment, yay!
+        for kk in zzz:
+            
+            adj_mat[ k , kk] = 1
+            adj_mat[ kk , k] = 1
+
+        
+        
+            
+# R code below here
+# do this for the number of iterations (50)
+# do this for each cell
+
+  
+  for (ii in 1:numcell){
+    adj_mat[ which( match(r, r[ii]) == 1)[1], which( match(r, r[ii]) == 1)[2] ] <- 1
+    adj_mat[ which( match(r, r[ii]) == 1)[2], which( match(r, r[ii]) == 1)[1] ] <- 1
+  }
+  
+}
