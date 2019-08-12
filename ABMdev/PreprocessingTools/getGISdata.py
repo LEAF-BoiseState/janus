c49715ec8e23eb2eeb04f89ed2a9c6c9fc43a118
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-08-12 15:58:17 -0600

    Works, but needs cleaned up

--- ABMdev/PreprocessingTools/getGISdata.py
+++ ABMdev/PreprocessingTools/getGISdata.py
@@ -18,11 +18,11 @@ from shapely.ops import cascaded_union
 import json
 
 #set user directory
-os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/')
-DataPath= '/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/'
-GCAMpath='/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/GIS_anlaysis/GCAM_SRP/'
-#os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/')
-#DataPath='/Users/kek25/Documents/GitRepos/IM3-BoiseState/'
+#os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/')
+#DataPath= '/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/'
+#GCAMpath='/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/GIS_anlaysis/GCAM_SRP/'
+os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/')
+DataPath='/Users/kek25/Documents/GitRepos/IM3-BoiseState/'
 GCAMpath='/Users/kek25/Dropbox/BSU/Python/IM3/GCAM_SRP/'
 
 counties_shp= gp.read_file('GIS_anlaysis/Shapefiles/County_polys/Counties_SRB_clip_SingleID.shp')
@@ -73,44 +73,44 @@ def countyID(countyList, lc):
     es=es.assign(value=np.arange(numCounties)) #add values to each shape
 
 #### none of this works
-    coords = [json.loads(extent_shp.to_json())['features'][0]['geometry']]#parses features from GeoDataFrame the way rasterio wants them... hypothetically
-    if numCounties > 1:
-        for i in np.arange(1, numCounties):
-            coords.append(json.loads(extent_shp.to_json())['features'][i]['geometry'])
+   # coords = [json.loads(extent_shp.to_json())['features'][0]['geometry']]#parses features from GeoDataFrame the way rasterio wants them... hypothetically
+    #if numCounties > 1:
+     #   for i in np.arange(1, numCounties):
+      #      coords.append(json.loads(extent_shp.to_json())['features'][i]['geometry'])
             
         #this version dont work
    #shapes=features.shapes(extent_shp) input to this has to be a rasterio object
    # coords[0]['coordinates'] - this is a list of 2 lists that have explicit coordinates of locations (pixels??)
-    shapes=features.shapes(coords[0]['coordinates']) #'list' object has no attribute 'dtype'
-    shapes=((geometry,value) for geometry, value in SOMETHING) #this creates a generator which is an iterable
+    #shapes=features.shapes(coords[0]['coordinates']) #'list' object has no attribute 'dtype'
+    #shapes=((geometry,value) for geometry, value in SOMETHING) #this creates a generator which is an iterable
     
-    outshape=(2809, 2838) # in the function it would be lc[0].shape
-    out=features.rasterize(shapes, outshape, fill=999, all_touched=False)
+    #outshape=(2809, 2838) # in the function it would be lc[0].shape
+    #out=features.rasterize(shapes, outshape, fill=999, all_touched=False)
     
 # this creates generator of geom, value pairs to use in rasterizing
-    shapes=((geometry,value) for geometry, value in es) #this creates a generator which is an iterable
+    #shapes=((geometry,value) for geometry, value in es) #this creates a generator which is an iterable
     #(es['geometry'], 0) this results in a tuple - geoseries, value pair, Invalid geometry object at index 0
     #(es['geometry'][0], 0) this results in a tuple - polygon, value pair, int is not iterable
-    out = features.rasterize(shapes=es, out_shape=outshape, fill=999, all_touched=False)
+    #out = features.rasterize(shapes=es, out_shape=outshape, fill=999, all_touched=False)
 
   
 
     
-    return(out) 
+    #return(out) 
 #------------------------------------------------------------------------
 # Select and save npy file of specific initialization year
 #------------------------------------------------------------------------
 
 countyList=['Ada', 'Canyon']  
 year=2010
-scale=1000
+scale=3000
 
 
-extent_poly=getGISextent(countyList, '1km')
+extent_poly=getGISextent(countyList, '3km')
 gcam_init=getGCAM(countyList, year, scale)
 
-extent_poly.to_file(DataPath+'ABMdev/Data/extent_1km_AdaCanyon.shp')
-np.save(DataPath+'ABMdev/Data/gcam_1km_2010_AdaCanyon.npy', gcam_init) #not sure if this one needs to be changed ...
+extent_poly.to_file(DataPath+'ABMdev/Data/extent_3km_AdaCanyon.shp')
+np.save(DataPath+'ABMdev/Data/gcam_3km_2010_AdaCanyon.npy', gcam_init) #not sure if this one needs to be changed ...
 
 
 
