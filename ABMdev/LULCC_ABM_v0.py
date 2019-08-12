636a265cfd35277c607106ccbc9b3a77b3a3ad34
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-08-12 11:50:49 -0600

    Changed initialization of Agents into functions in InitializeAgentsDomain

--- ABMdev/LULCC_ABM_v0.py
+++ ABMdev/LULCC_ABM_v0.py
@@ -1,43 +1,11 @@
-# Outline of algorithm...
+"""
+Agent Based Model of Land Use and Land Cover Change 
 
-# 0. Declare some key variables
-#   - Name of spatial domain map with LULCC classes 
-#   - Name of spatial domain map with active/inactive cells (e.g., national forests, etc.)
-#   - Name of spatial domain map of urban expansion areas
-#   - Name of spatial domain map of any ag attributes needed (e.g., type of ag - leased etc.)
-#   - Number of simulation years or time steps
-#   - Output interval
-#   - Output base name
-
-# 1. Preprocessing
-#   - Load spatial maps
-#   - Set up any agent classes for spatial domains
-#   - Create any temporary, derived spatial domains
-#   - Create storage containers for LULCC simulations (NaNs outside active domain?)
-#   - Create any exogenous agent types needed (e.g., regulators, global markets)
-
-# 2. Simulation loop
-#   - Loop through time
-#       > Do any needed statistics at the beginning of time (i.e., update neighborhood stats for farmers, urban areas, etc.)
-#       > Get any new global info needed (i.e., value of crops, number of potential developers in reservoir)
-#       > Get all farmer types
-#           - Compute happiness metric based on urbanness, global crop price
-#           - Unhappy agents:
-#               > If in urban expansion zone, choose between: stay, sell, switch to corporate
-#               > If not in urban expansion zone, choose between: stay, sell (exurb), switch to corporate 
-#       > Decrement urban types depending on number of farmers that sell
-#       > Decrement exurban types developing on number of farmers that sell
-#       > Urban fraction inside development zone > threshold?
-#           - Yes: expand urban zone to accommodate 20 years' growth (this actually comes from COMPASS)
-#           - No: Do nothing
-#       > Add new urban types for next year
-#       > Add new exurban types for next year
-
-# 3. Save output
-#   - Aggregate statistics through time (csv or NetCDF)
-#   - Spatial land use patterns through time (NetCDF)
-
-
+@author: lejoflores & kendrakaiser
+"""
+#---------------------------------------
+#  Load Packages
+#---------------------------------------
 import geopandas as gp
 import numpy as np
 from geofxns import minDistCity #slow
@@ -46,20 +14,14 @@ import Classes.aFarmer as farmer
 import Classes.dCellClass as cell
 import Classes.aUrban as urban
 import CropFuncs.CropDecider as cd
+import InitializeAgentsDomain as init
 
 userPath='/Users/kek25/Documents/GitRepos/'
 DataPath= userPath+'IM3-BoiseState/'
 
-#load extent
-extent=gp.read_file(DataPath + 'ABMdev/Data/extent_1km_AdaCanyon.shp')
-#load inital landcover
-lc=np.load(DataPath + 'ABMdev/Data/gcam_1km_2010_AdaCanyon.npy')
-#initalize minimum distance to city
-dist2city=minDistCity(lc)
-
-Ny, Nx = lc[0].shape
-Nt = 50
-
+#---------------------------------------
+# 0. Declare Variables
+#---------------------------------------
 #set agent switching parameters
 a_ra = 4.5
 b_ra = 1.0
@@ -69,6 +31,18 @@ fmax = 1.5
 f0 = 1.2
 n = 100
 
+#---------------------------------------
+# 1. Preprocessing
+#---------------------------------------
+#load extent
+extent=gp.read_file(DataPath + 'ABMdev/Data/extent_1km_AdaCanyon.shp')
+#load inital landcover
+lc=np.load(DataPath + 'ABMdev/Data/gcam_1km_2010_AdaCanyon.npy')
+#initalize minimum distance to city
+dist2city=minDistCity(lc)
+
+Ny, Nx = lc[0].shape
+Nt = 10
 #---------------------------------------
 #  Initialize Crops
 #---------------------------------------
@@ -83,57 +57,35 @@ CropID_all[0,:,:] = lc
 Profit_ant = np.zeros((Nt,Ny,Nx))
 Profit_act = np.zeros((Nt,Ny,Nx))
 
-Profit_ant[0,:,:] = 30000.0 + np.random.normal(loc=0.0,scale=1000.0,size=(1,Nx,Ny))
+Profit_ant[0,:,:] = 30000.0 + np.random.normal(loc=0.0,scale=1000.0,size=(1,Ny,Nx))
 Profit_act[0,:,:] = Profit_ant[0,:,:]
 
 P = [] # A list of numpy arrays that will be Nt x Nc
 for i in np.arange(Ny):
     for j in np.arange(Nx):
-        P.append(cd.GeneratePrices(Nt))
+        P.append(cd.GeneratePrices(Nt)) #slow
 
 #---------------------------------------
 #  Initialize Agents
 #---------------------------------------
-AgentArray = np.empty((Ny,Nx),dtype='U10')
-dFASM = np.empty((Ny,Nx), dtype=object) #domain 
-
-for i in np.arange(Ny):
-	for j in np.arange(Nx):
-		dFASM[i][j] = cell.dCellClass()
-
 #Update so each of these inital values randomly selected from NASS distributions
-#Initialize farmer
-AgeInit = int(45.0)
-nFields=1
-AreaFields=np.array([10])
-LandStatus=0
-density=2
+AgentData = {
+        "AgeInit" : int(45.0),
+        "nFields" : 1,
+        "AreaFields" : np.array([10]),
+        "LandStatus" : 0,
+        "density" : 2,
+        }
+
+AgentArray, dFASM = init.InitializeDomain(Ny,Nx)
+AgentArray = init.PlaceAgents(AgentArray, lc, dist2city)
+dFASM = init.InitializeAgents(AgentArray, AgentData, dFASM, dist2city, Ny, Nx)
 
 #---------------------------------------
-#  assign agents on the landscape 
-#---------------------------------------
-AgentArray[np.logical_and(lc[0] > 0, lc[0] <28)] = 'aFarmer'
-AgentArray[np.logical_or(lc[0] == 28, lc[0] == 23)] ='water' 
-AgentArray[dist2city == 0] = 'aUrban'
-AgentArray[np.logical_or(lc[0] == 24, lc[0] == 21)] = 'empty' #RockIceDesert, Shrubland
-AgentArray[np.logical_or(lc[0] == 19, lc[0] == 15)] = 'empty' #forest, pasture
-
-#---------------------------------------
-#place agent structures onto landscape and define attributes -> this is SLOW
+#loop through decision process - change into function?
 #---------------------------------------
 
-for i in np.arange(Ny):
- 	for j in np.arange(Nx):
-         if(AgentArray[i][j]=='aFarmer'):
-             NewAgent = farmer.aFarmer(AgeInit, nFields, AreaFields, LandStatus, dist2city[i][j])
-             dFASM[i][j].AddAgent(AgentArray[i][j], NewAgent)
-         if(AgentArray[i][j] =='aUrban'):
-             NewAgent = urban.aUrban(density)
-             dFASM[i][j].AddAgent(AgentArray[i][j], NewAgent)
-        
-#---------------------------------------
-#loop through decision process
-#---------------------------------------
+
 for i in np.arange(1,Nt):
     for j in np.arange(Nx):
         for k in np.arange(Ny):
@@ -163,7 +115,7 @@ for i in np.arange(1,Nt):
                 Profit_act[i,j,k] = Profit_ant[i,j,k] + np.random.normal(loc=0.0, scale=1000.0, size=(1,1,1))
 
             
-            
+         #update distance to city   
             
 #Update AgentArray 
 #where in the model does the code denote that the agent goes from farmer to urban or visa versa
@@ -171,7 +123,7 @@ for i in np.arange(1,Nt):
         
 temp_lc= lc #output of decision process
 
-#write landcover to array
+#write landcover to array - sub w Jons work
 saveLC(temp_lc, 2010, it, DataPath)
            
 
