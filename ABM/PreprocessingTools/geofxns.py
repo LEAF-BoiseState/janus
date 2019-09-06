ce147eb165a26d0d38bcb5874b6af88989aff6e7
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-09-06 16:35:01 -0600

    Changed grid2poly into function and added to geofxns

--- ABM/PreprocessingTools/geofxns.py
+++ ABM/PreprocessingTools/geofxns.py
@@ -11,8 +11,11 @@ minDistCity - Calculates the distance from any cell to a city cell of any densit
 """
 import numpy as np
 import geopandas as gp
+import gdal
 import rasterio
 from rasterio.mask import mask
+from shapely.geometry import Polygon, MultiPolygon 
+from fiona.crs import from_epsg
 
 import pycrs
 from shapely.ops import cascaded_union
@@ -25,9 +28,45 @@ GCAMpath='~Data/GCAM_SRP/'
 counties_shp= gp.read_file('~Data/Counties/Counties_SRB_clip_SingleID.shp')
 counties_shp=counties_shp.set_index('county')
 
-#------------------------------------------------------------------------
+#----------------------------------------------------------------------------
 # DEFINE FUNCTIONS
-#------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------
+# Create a set of polygons for entire domain
+#----------------------------------------------------------------------------
+
+def grid2poly(grid_file, OutFileName):
+    
+    src= gdal.Open(DataPath+grid_file)
+    srcarray = src.ReadAsArray().astype(np.float)
+
+    x_index =np.arange(srcarray.shape[1]) 
+    y_index = np.arange(srcarray.shape[0])
+    (upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size) = src.GetGeoTransform()
+    x_coords = x_index * x_size + upper_left_x + (x_size / 2) #add half the cell size
+    y_coords = y_index * y_size + upper_left_y + (y_size / 2) #to centre the point
+    xc, yc = np.meshgrid(x_coords, y_coords)
+
+    #create a list of all the polygons in the grid
+    vert = list()
+    for i in np.arange(srcarray.shape[1]-1):  
+        for j in np.arange(srcarray.shape[0]-1):  
+                vert.append([[xc[j, i] , yc[j,i]], [xc[j+1, i], yc[j+1, i]], [xc[j+1, i+1], yc[j+1, i+1]],[xc[j, i+1], yc[j, i+1]]])
+ 
+    #create list of polygons
+    polygons=[Polygon(vert[i]) for i in np.arange(len(vert))]
+
+    #convert them to formats for exporting 
+    polys   = gp.GeoSeries(MultiPolygon(polygons))
+    polyagg = gp.GeoDataFrame(geometry=polys)
+    polyagg.crs= from_epsg(32611)
+
+    #-------------------------#
+    # Save Output             #
+    #-------------------------#
+    polyagg.to_file(filename=DataPath+OutFileName, driver="ESRI Shapefile")
+
 
 
 #----------------------------------------------------------------------------
