d6c8437d00c5c1fa6805bd28a6d07eaabfbfeb62
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-09-06 15:44:21 -0600

    Reorganizing data and input strings

--- GIS_anlaysis/AggregateGCAMGrids.py
+++ GIS_anlaysis/AggregateGCAMGrids.py
@@ -19,23 +19,18 @@ AggRes = 3000.0 # In units of m
 #=============================================================================#
 # Set master working  directories                                             #
 #=============================================================================#
-# Base user directories
-#lejo = '/Users/lejoflores/IM3-BoiseState/CDL_analysis/'
-#user = lejo
 
 # Specific directories of where to find the data
-GCAM_ReadDir = '/Users/kek25/Dropbox/BSU/Python/IM3/GCAM_SRP/'
+GCAM_Dir = '~/Data/GCAM/'
 
-GCAM_ReadFiles = glob.glob(GCAM_ReadDir +'gcam*srb.tiff')
-
-GCAM_ReprojWriteDir  = GCAM_ReadDir + '/3km/'
+GCAM_ReadFiles = glob.glob(GCAM_Dir +'gcam*srb.tiff')
 
 #=============================================================================#
 # FUNCTION DEFINITIONS    
-def AggregateGCAMGrid(GCAM_ReadDir,GCAM_ReadFile,GCAM_WriteDir,AggRes):
+def AggregateGCAMGrid(GCAM_ReadWriteDir,GCAM_ReadFile, AggRes):
     
     # Open the GeoTiff based on the input path and file
-    src_ds = gdal.Open(GCAM_ReadDir+GCAM_ReadFile)
+    src_ds = gdal.Open(GCAM_ReadWriteDir+GCAM_ReadFile)
 
     # Create the name of the output file by modifying the input file
     GCAM_WriteFile = GCAM_ReadFile.replace('srb','srb'+'_'+str(int(AggRes)))
@@ -54,11 +49,11 @@ def AggregateGCAMGrid(GCAM_ReadDir,GCAM_ReadFile,GCAM_WriteDir,AggRes):
     dst_nrows = (int)(src_nrows/agg_factor)
 
     dst_driver = gdal.GetDriverByName('Gtiff')
-    dst_ds = dst_driver.Create(GCAM_WriteDir+GCAM_WriteFile, dst_ncols, dst_nrows, 1, gdal.GDT_Float32)
+    dst_ds = dst_driver.Create(GCAM_ReadWriteDir+GCAM_WriteFile, dst_ncols, dst_nrows, 1, gdal.GDT_Float32)
 
     dst_geot = (src_geot[0], src_geot[1]*agg_factor, src_geot[2], src_geot[3], src_geot[4], src_geot[5]*agg_factor)
 
-    dst_ds.SetGeoTransform(dst_geot) ##dst_ds is not correct
+    dst_ds.SetGeoTransform(dst_geot)
     dst_ds.SetProjection(src_proj)
 
     gdal.ReprojectImage(src_ds, dst_ds, src_proj, src_proj, gdal.GRA_Mode)
@@ -69,5 +64,5 @@ def AggregateGCAMGrid(GCAM_ReadDir,GCAM_ReadFile,GCAM_WriteDir,AggRes):
     return
 #                                                                             #
 #=============================================================================#
-Parallel(n_jobs=4, verbose=60, backend='threading')(delayed(AggregateGCAMGrid)(GCAM_ReadDir,os.path.basename(file),GCAM_ReprojWriteDir,AggRes) \
+Parallel(n_jobs=4, verbose=60, backend='threading')(delayed(AggregateGCAMGrid)(GCAM_Dir,os.path.basename(file),AggRes) \
          for file in GCAM_ReadFiles)
