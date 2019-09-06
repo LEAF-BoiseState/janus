11bfb0b3b301a803bdedcb9853c2894869a5fc8e
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-09-06 15:52:24 -0600

    Cleaning up data paths

--- ABM/PreprocessingTools/getGISdata.py
+++ ABM/PreprocessingTools/getGISdata.py
@@ -7,25 +7,21 @@ Created on Mon Apr  8 21:55:00 2019
 
 function that clips GIS data based on counties, year, and resolution
 """
-import os  
+ 
 import geopandas as gp
 import numpy as np
 import rasterio
 from rasterio.mask import mask
-from rasterio import features
+
 import pycrs
 from shapely.ops import cascaded_union
 import json
 
-#set user directory
-#os.chdir('/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/')
-#DataPath= '/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/'
-#GCAMpath='/Users/kendrakaiser/Documents/GitRepos/IM3-BoiseState/GIS_anlaysis/GCAM_SRP/'
-os.chdir('/Users/kek25/Documents/GitRepos/IM3-BoiseState/')
-DataPath='/Users/kek25/Documents/GitRepos/IM3-BoiseState/'
-GCAMpath='/Users/kek25/Dropbox/BSU/Python/IM3/GCAM_SRP/'
+#set paths
+DataPath='~Data/'
+GCAMpath='~Data/GCAM_SRP/'
 
-counties_shp= gp.read_file('GIS_anlaysis/Shapefiles/County_polys/Counties_SRB_clip_SingleID.shp')
+counties_shp= gp.read_file('~Data/Counties/Counties_SRB_clip_SingleID.shp')
 counties_shp=counties_shp.set_index('county')
 
 #------------------------------------------------------------------------
@@ -35,9 +31,9 @@ counties_shp=counties_shp.set_index('county')
 def getGISextent(countyList, scale):
     
     if scale == '3km':
-        SRB_poly= gp.read_file(DataPath+'GIS_anlaysis/Shapefiles/SRB_gridpolys/SRB_poly_3km_V2.shp') 
+        SRB_poly= gp.read_file(DataPath+'SRB_poly_3km.shp') 
     elif scale == '1km':
-        SRB_poly= gp.read_file(DataPath+'GIS_anlaysis/Shapefiles/SRB_gridpolys/SRB_poly_1km_V2.shp') 
+        SRB_poly= gp.read_file(DataPath+'SRB_poly_1km.shp') 
     
     #select two shapefiles, this returns geometry of the union - this no longer distinguishes two
     extent=counties_shp['geometry'].loc[countyList].unary_union #this is the row index, not the "COUNTY_ALL" index
@@ -61,10 +57,10 @@ def getGCAM(countyList, year, scale): #returns a numpy array
                  "transform": out_transform,
                  "crs": pycrs.parse.from_epsg_code(epsg_code).to_proj4()}
                         )
-    return(out_img) #this results in a 3D shape?
+    return(out_img) 
 
 
-#tryin to figureout hoew to turn the shapefile with two counties into a raster   
+#trying to  turn the shapefile with two counties into a raster   
 def countyID(countyList, lc):
     
     extent_shp=counties_shp['geometry'].loc[countyList]#this is a geoseries, figuerout how to save the value that can go into the raster
@@ -109,8 +105,8 @@ scale=3000
 extent_poly=getGISextent(countyList, '3km')
 gcam_init=getGCAM(countyList, year, scale)
 
-extent_poly.to_file(DataPath+'ABMdev/Data/extent_3km_AdaCanyon.shp')
-np.save(DataPath+'ABMdev/Data/gcam_3km_2010_AdaCanyon.npy', gcam_init) #not sure if this one needs to be changed ...
+extent_poly.to_file(DataPath+'extent_3km_AdaCanyon.shp')
+np.save(DataPath+'gcam_3km_2010_AdaCanyon.npy', gcam_init) #not sure if this one needs to be changed ...
 
 
 
