da0dba78fc06254f233f4955375cc4e03af5e544
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-09-17 21:08:01 -0600

    Woking on adding agents not working

--- ABM/Classes/aFarmer.py
+++ ABM/Classes/aFarmer.py
@@ -10,29 +10,35 @@ import numpy as np
 class aFarmer:
     
     def __init__(self,**kwargs):
-        self._Validate(kwargs)
+        #self._Validate(kwargs)
         self.Age = kwargs.get('Age')
-        self.nFields = kwargs.get('nFields')
-        self.AreaFields = kwargs.get('AreaFields')
-        self.LandStatus = kwargs.get('LandStatus')
         self.Dist2city = kwargs.get('Dist2city')
-        self.AreaFields = kwargs.get('AreaFields')
-
-        self.AreaFields = np.nan*np.ones(self.AreaFields.shape)
-        for i in np.arange(self.nFields):
-            self.AreaFields[i] = self.AreaFields[i]
-
+        
+        self.LandStatus = kwargs.get('LandStatus')
         self.LandStatus = self.LandStatus
+        
+        self.nFields = kwargs.get('nFields')
+        self.nFields = self.nFields
+        
+        self.alpha = kwargs.get('alpha') #write asserts that require the value to be between 0/1 ?
+        self.beta = kwargs.get('beta')
+        
+        #self.AreaFields = kwargs.get('AreaFields')
+        #self.AreaFields = np.nan*np.ones(self.AreaFields.shape)
+        #for i in np.arange(self.nFields):
+         #   self.AreaFields[i] = self.AreaFields[i]
+            
+        
 
     def UpdateAge(self):
-        self.Age += 1
+        self.Age += 12
 
     def UpdateDist2city(self, newDist):
         self.Dist2city = newDist
 
 ############### Private Functions ###############
-    @staticmethod
-    def _Validate(self, **kwargs):
-        if type(self.nFields)!=int: raise Exception("nFields is not valid: Invalid Type")
-        if self.AreaFields.size != self.nFields: raise Exception("nFields is not valid: Invalid Size")
-        if self.LandStatus == 0 or self.LandStatus == 1 or self.LandStatus == 2: raise Exception("LandStatus is not valid: Invalid Status")
+   # @staticmethod
+   # def _Validate(self, **kwargs):
+       # if type(self.nFields)!=int: raise Exception("nFields is not valid: Invalid Type")
+        #if self.AreaFields.size != self.nFields: raise Exception("nFields is not valid: Invalid Size")
+       # if self.LandStatus != 0 and self.LandStatus != 1 and self.LandStatus != 2: raise Exception("LandStatus is not valid: Invalid Status")
--- ABM/InitializeAgentsDomain.py
+++ ABM/InitializeAgentsDomain.py
@@ -9,7 +9,6 @@ import numpy as np
 import Classes.aFarmer as farmer
 import Classes.dCellClass as cell
 import Classes.aUrban as urban
-import pandas as pd 
 
 def InitializeDomain(Ny,Nx):
     "Initialize domain"
@@ -22,7 +21,7 @@ def InitializeDomain(Ny,Nx):
         #whats a unit test for this??"
     return (dFASM)
 
-def PlaceAgents(Ny,Nx, lc, dist2city, key_file, cat_option):
+def PlaceAgents(Ny,Nx, lc, key_file, cat_option):
     #assert that cat_option has to be a header in the csv doc
     AgentArray = np.empty((Ny,Nx),dtype='U10')
     
@@ -59,7 +58,7 @@ def InitializeAgents(AgentArray, AgentData, dFASM, dist2city, Ny, Nx):
     for i in np.arange(Ny):
         for j in np.arange(Nx):
             if(AgentArray[i][j]=='aFarmer'):
-                 NewAgent = farmer.aFarmer(AgentData["AgeInit"], AgentData["LandStatus"], dist2city[i][j]) #this is passing actual agent data
+                 NewAgent = farmer.aFarmer(Age=AgentData["AgeInit"], LandStatus=AgentData["LandStatus"], Dist2city=dist2city[i][j], nFields=1, AreaFields=3000) #this is passing actual agent data
                  dFASM[i][j].AddAgent(AgentArray[i][j], NewAgent)
             if(AgentArray[i][j] =='aUrban'):
                 NewAgent = urban.aUrban(AgentData["density"])
