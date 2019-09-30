d1f2d592e0a8dc5523a028a3c34b2a91fe237e50
crvernon <chrisrvernon@gmail.com>
2019-09-30 13:25:00 -0400

    alter to class

--- abm/model.py
+++ abm/model.py
@@ -25,144 +25,250 @@ class Abm:
 
         self.key_file = gp.read_file(c['f_key_file'], sep=',')
 
-        # TODO:  start here adding additional variables from the config file that are used in this script
+        self.Nt = c['nt']
 
-        # TODO:  add checks for input variables to confirm validity
+        # TODO: there are actually 17 when the 1km is run, need random profit profiles for each of these
+        self.Nc = c['nc']
+
+        # set agent switching parameters (alpha, beta) [[switching averse], [switching tolerant]]
+        self.switch = np.array(c['switch_params'])
+
+        # proportion of each switching type, lower than p is averse, higher is tolerant
+        self.p = c['p']
+
+        # Max and min .... total Profit, percent profit?
+        self.fmin = c['fmin']
+        self.fmax = c['fmax']
+        self.f0 = c['f0']
+        self.n = c['n']
+
+        # TODO:  define seed for crop decider; This is not used in this script but is set as `global`
+        crpdec.DefineSeed(c['crop_seed_size'])
+
+        # target year
+        self.target_year = c['target_year']
+
+        # scale of grid in meters
+        self.scale = c['scale']
+
+        # list of counties to evaluate
+        self.county_list = c['county_list']
+
+        # agent variables
+        self.agent_variables = c['agent_variables']
+
+        # NASS year
+        self.nass_year = c['nass_year']
+
+        # NASS county list
+        self.nass_county_list = [i.upper() for i in c['nass_county_list']]
+
+        # initialize landscape and domain
+        self.lc, self.dist2city, self.domain, self.Ny, self.Nx = self.initialize_landscape_domain()
+
+        # initialize crops
+        self.crop_ids, self.crop_id_all = self.initialize_crops()
+
+        # initialize profits
+        self.profit_act, self.profit_ant, self.profits = self.initialize_profit()
+
+        # initialize agents
+        self.agent_domain, self.agent_array = self.initialize_agents()
+
 
     @staticmethod
     def config_reader(config_file):
-        """Read the YAML config file to a dictionary."""
+        """Read the YAML config file to a dictionary.
+
+        :param config_file:             Full path with file name and extension to the input config file.
+
+        :return:                        YAML dictionary-like object
+
+        """
 
         with open(config_file) as f:
             return yaml.safe_load(f)
 
+    def initialize_landscape_domain(self):
+        """Initialize landscape and domain.
 
-Nt = 50
-# set agent switching parameters (alpha, beta)
-switch = np.array([[4.5, 1.0],  # switching averse
-                   [0.5, 3.0]])  # switching tolerant
-# proportion of each switching type, lower than p is averse, higher is tolerant
-p = 0.5
+        :return:                        TODO:  add return descriptions for each variable
 
-# Max and min .... total Profit, percent profit?
-fmin = 1.0
-fmax = 1.5
-f0 = 1.2
-n = 100
+        """
 
-crpdec.DefineSeed(5)
+        # select initial gcam data from initial year
+        lc = gf.getGCAM(self.counties_shp, self.county_list, self.target_year, self.scale, GCAMpath)
 
-# ---------------------------------------
-# 1. Initialize Landscape and Domain
-# ---------------------------------------
+        ny, nx = lc[0].shape
 
-countyList = ['Ada', 'Canyon']
-year = 2010
-scale = 3000  # scale of grid in meters
+        # initialize minimum distance to city
+        dist2city = gf.minDistCity(lc)
 
-# select initial gcam data from initial year
-lc = gf.getGCAM(counties_shp, countyList, year, scale, GCAMpath)
+        domain = init_agent.InitializeDomain(ny, nx)
 
-# initialize minimum distance to city
-dist2city = gf.minDistCity(lc)
+        return lc, dist2city, domain, ny, nx
 
-Ny, Nx = lc[0].shape
+    def initialize_profit(self):
+        """Initialize crops
 
-domain = init_agent.InitializeDomain(Ny, Nx)
-# ---------------------------------------
-#  Initialize Crops
-# ---------------------------------------
-Nc = 4  # there are actually 17 when the 1km is run, need random profit profiles for each of these
-CropIDs = np.array([1, 2, 3, 10])  # need to make this automatic depending on which crops show up (which of AllCropIDs == np.unique(lc))
-CropIDs = CropIDs.reshape((Nc, 1))
-# CropIDs=np.arange(Nc).reshape((Nc,1)) + 1
-CropID_all = np.zeros((Nt, Ny, Nx))
-CropID_all[0, :, :] = lc  # this will be added into the cell class
+        :return:                        TODO: add return descriptions for each variable
 
-# ---------------------------------------
-#  Initialize Profits
-# ---------------------------------------
-Profit_ant = np.zeros((Nt,Ny,Nx))
-Profit_act = np.zeros((Nt,Ny,Nx))
+        """
 
-Profit_ant[0,:,:] = 30000.0 + np.random.normal(loc=0.0,scale=1000.0,size=(1,Ny,Nx))
-Profit_act[0,:,:] = Profit_ant[0,:,:]
+        # TODO: need to make this automatic depending on which crops show up (which of AllCropIDs == np.unique(lc))
+        crop_ids = np.array([1, 2, 3, 10]).reshape(self.Nc, 1)
 
-Profits = [] # A list of numpy arrays that will be Nt x Nc 
-Profits = crpdec.GeneratePrices(Nt)
-Profits = Profits[:, 0:Nc]
-# Profits = Profits[:, 2:6] The choice of crop profits will completely drive the outcome ... how do we use that?
+        crop_id_all = np.zeros((self.Nt, self.Ny, self.Nx))
 
-# ---------------------------------------
-#  Initialize Agents
-# ---------------------------------------
+        # TODO: this will be added into the cell class
+        crop_id_all[0, :, :] = self.lc
 
-variables=["TENURE", "AREA OPERATED"]
-NASS_yr=2007  # 2007, 2012 are available
-NASS_countyList=['ADA', 'CANYON']  # these have to be capitalized
-tenure = get_nass.TenureArea('ID', NASS_countyList, NASS_yr, variables)  # tenure from individual counties can also be used
-ages = get_nass.Ages(NASS_yr, 'ID')
+        return crop_ids, crop_id_all
 
-AgeCDF = get_nass.makeAgeCDF(ages)
-TenureCDF = get_nass.makeTenureCDF(tenure)
+    def initalize_profits(self, unknown_var=30000.0, scale=1000.0):
+        """Initialize profits.
 
-AgentArray = init_agent.PlaceAgents(Ny, Nx, lc, key_file, 'SRB')
+        :return:                        TODO:  add return descriptions for each variable
 
-domain = init_agent.InitializeAgents(AgentArray, domain, dist2city, TenureCDF, AgeCDF, switch, Ny, Nx, lc, p)
+        """
 
-# ---------------------------------------
-# 2. loop through decision process 
-# ---------------------------------------
+        profit_ant = np.zeros((self.Nt, self.Ny, self.Nx))
+
+        # TODO: what is the unknown variable??  Where does scale in this case come from??
+        profit_ant[0, :, :] = unknown_var + np.random.normal(loc=0.0, scale=scale, size=(1, self.Ny, self.Nx))
+
+        profit_act = profit_ant.copy()
+
+        profits = crpdec.GeneratePrices(self.Nt)[:, 0:self.Nc]
+
+        return profit_act, profit_ant, profits
+
+    def initialize_agents(self, id_field='ID', cat_header='SRB'):
+        """Initialize agents.
 
 
-for i in np.arange(1,Nt):
-    
-    for j in np.arange(Ny):
+        :return:                        TODO:  add return descriptions for each variable
 
-        for k in np.arange(Nx):
+        """
 
-            if domain[j,k].FarmerAgents:
+        # tenure from individual counties can also be used
+        tenure = get_nass.TenureArea(id_field, self.nass_county_list, self.nass_year, self.agent_variables)
 
-                # Assess Profit
-                Profit_last, Profit_pred = crpdec.AssessProfit(CropID_all[i-1, j, k], Profits[i-1,:], Profits[i,:], Nc, CropIDs)
+        ages = get_nass.Ages(self.nass_year, id_field)
 
-                # Decide on Crop
-                CropChoice, ProfitChoice = crpdec.DecideN(domain[j,k].FarmerAgents[0].alpha, domain[j,k].FarmerAgents[0].beta, fmin, fmax, n, Profit_last, CropIDs, Profit_pred, rule=True)
-                CropID_all, Profit_ant, Profit_act = crpdec.MakeChoice(CropID_all[i-1,j,k], Profit_last, Profit_pred, CropChoice, ProfitChoice, Profit_act)
+        age_cdf = get_nass.makeAgeCDF(ages)
 
-                # move these indicies into the input variables
-                CropChoice, ProfitChoice = crpdec.DecideN(domain[j,k].FarmerAgents[0].alpha, domain[j,k].FarmerAgents[0].beta, fmin, fmax, n, Profit_last, CropIDs, Profit_pred, rule=True)
-                CropID_all[i,j,k], Profit_ant[i,j,k], Profit_act[i,j,k] = crpdec.MakeChoice(CropID_all[i-1,j,k], Profit_last, Profit_ant, CropChoice, ProfitChoice, seed = False) #is there a way to set this up so you can pass a NULL value or no value when seed=False?
- 
-ppf.CropPerc(CropID_all, CropIDs, Nt, Nc)
-#ppf.CreateAnimation(CropID_all, Nt)
-#CropID_all, Profit_ant, Profit_act = cd.MakeDecision(Nt, Ny, Nx, Nc, CropID_all, Profits, Profit_ant, Profit_act, a_ra, b_ra, fmin, fmax, n, CropIDs)
-#"one unit test would be to confirm that non-ag stayed the same and that all of the ag did not stay the same"        
-#need to pull out the parts that dont rely on the loop and put the decision inside of it, that way relevant info can be updated between timesteps; 
+        tenure_cdf = get_nass.makeTenureCDF(tenure)
 
-FarmerAges = ppf.AgentAges(domain, AgentArray, Ny, Nx)
-#---------------------------------------
-# 3. update variables 
-#---------------------------------------
+        agent_array = init_agent.PlaceAgents(self.Ny, self.Nx, self.lc, self.key_file, cat_header)
 
-#update distance to city from output of decision process - IF we were incorperating the urban agent then this would happen in each time step
-#dist2city=minDistCity(lc_new) 
-     
-#Update AgentArray
-#where in the model does the code denote that the agent goes from farmer to urban or visa versa
-     #domain[i][j].SwapAgent('aFarmer','aUrban',fromIndex,AgentArray) "switch for now"
-     
-for i in np.arange(Ny):
-    for j in np.arange(Nx):
-         if AgentArray[i][j]=='aFarmer':
-             domain[i][j].FarmAgents[0].UpdateAge()
-             domain[i][j].FarmAgents[0].UpdateDist2city(dist2city[i][j])
-      
-# ---------------------------------------
-# 4. Save Output
-# ---------------------------------------
-      
-#write landcover to array - sub w Jons work
-#saveLC(temp_lc, 2010, it, DataPath)
+        agent_domain = init_agent.InitializeAgents(agent_array, self.domain, self.dist2city, tenure_cdf, age_cdf,
+                                                   self.switch, self.Ny, self.Nx, self.lc, self.p)
+
+        return agent_domain, agent_array
+
+    def decisions(self):
+        """Decision process.
+
+        :return:                        TODO:  add return descriptions for each variable
+
+        """
+
+        for i in np.arange(1, self.Nt):
+
+            for j in np.arange(self.Ny):
+
+                for k in np.arange(self.Nx):
+
+                    if self.agent_domain[j, k].FarmerAgents:
+
+                        # Assess Profit
+                        profit_last, profit_pred = crpdec.AssessProfit(self.crop_id_all[i - 1, j, k],
+                                                                       self.profits[i - 1, :],
+                                                                       self.profits[i, :],
+                                                                       self.Nc,
+                                                                       self.crop_ids)
+
+                        # Decide on Crop
+                        crop_choice, profit_choice = crpdec.DecideN(self.agent_domain[j, k].FarmerAgents[0].alpha,
+                                                                      self.agent_domain[j, k].FarmerAgents[0].beta,
+                                                                      self.fmin,
+                                                                      self.fmax,
+                                                                      self.n,
+                                                                      profit_last,
+                                                                      self.crop_ids,
+                                                                      profit_pred,
+                                                                      rule=True)
+
+                        crop_id_all, profit_ant, profit_act = crpdec.MakeChoice(self.crop_id_all[i - 1, j, k],
+                                                                                profit_last,
+                                                                                profit_pred,
+                                                                                crop_choice,
+                                                                                profit_choice,
+                                                                                self.profit_act)
+
+                        # move these indicies into the input variables
+                        crop_choice, profit_choice = crpdec.DecideN(self.agent_domain[j, k].FarmerAgents[0].alpha,
+                                                                  self.agent_domain[j, k].FarmerAgents[0].beta,
+                                                                  self.fmin,
+                                                                  self.fmax,
+                                                                  self.n,
+                                                                  profit_last,
+                                                                  self.crop_ids,
+                                                                  profit_pred,
+                                                                  rule=True)
+
+                        # is there a way to set this up so you can pass a NULL value or no value when seed=False?
+                        crop_id_all[i, j, k], profit_ant[i, j, k], profit_act[i, j, k] = crpdec.MakeChoice(
+                                                                                            self.crop_id_all[i - 1, j, k],
+                                                                                            profit_last,
+                                                                                            self.profit_ant,
+                                                                                            crop_choice,
+                                                                                            profit_choice,
+                                                                                            seed=False)
+
+        ppf.CropPerc(self.crop_id_all, self.crop_ids, self.Nt, self.Nc)
+
+        # crop_id_all, profit_ant, profit_act = cd.MakeDecision(Nt, Ny, Nx, Nc, crop_id_all, profits, profit_ant,
+        # profit_act, a_ra, b_ra, fmin, fmax, n, crop_ids)
+
+        # one unit test would be to confirm that non-ag stayed the same and that all of the ag did not stay the same"
+        # need to pull out the parts that dont rely on the loop and put the decision inside of it, that way
+        # relevant info can be updated between timesteps;
+
+        # TODO:  where is FarmerAges used?
+        FarmerAges = ppf.AgentAges(self.agent_domain, self.agent_array, self.Ny, self.Nx)
+
+    def update(self):
+        """Update agent variables.
+
+        :return:
+
+        """
+
+        # Update self.agent_array
+        # where in the model does the code denote that the agent goes from farmer to urban or visa versa
+             # agent_domain[i][j].SwapAgent('aFarmer','aUrban',fromIndex,AgentArray) "switch for now"
+
+        for i in np.arange(self.Ny):
+
+            for j in np.arange(self.Nx):
+
+                if self.agent_array[i][j]=='aFarmer':
+
+                    self.agent_domain[i][j].FarmAgents[0].UpdateAge()
+
+                    # TODO:  are you overwriting the previous value?
+                    self.agent_domain[i][j].FarmAgents[0].UpdateDist2city(self.dist2city[i][j])
+
+    def save_output(self):
+        """Save output
+
+
+        """
+
+        # write landcover to array - sub w Jons work
+
+        # saveLC(temp_lc, 2010, it, DataPath)
            
 
