1deac4459d79a6883cafccc34e8347b6d31e10f0
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-10-01 18:16:40 -0600

    Updated profit signals to be initialized off of NewSyntheticOutput.csv

--- ABM/LULCC_ABM_v0.py
+++ ABM/LULCC_ABM_v0.py
@@ -13,11 +13,12 @@ os.chdir(userPath+'IM3-BoiseState/ABM')
 
 import numpy as np
 import PreprocessingTools.geofxns as gf
-import CropFuncs.CropDecider as cd
+import CropFuNum_cropss.CropDecider as cd
 import InitializeAgentsDomain as init
 import PostProcessing.FigureFuncs as ppf
 import PreprocessingTools.getNASSAgentData as getNASS
 import geopandas as gp
+import pandas as pd
 
 DataPath= userPath+'IM3-BoiseState/Data/'
 GCAMpath=DataPath+'GCAM/'
@@ -25,8 +26,10 @@ GCAMpath=DataPath+'GCAM/'
 
 counties_shp= gp.read_file(DataPath+'Counties/Counties_SRB_clip_SingleID.shp')
 counties_shp=counties_shp.set_index('county')
-key_file= gp.read_file(DataPath+'CDL2GCAM_SRP_categories.csv', sep=',')
+key_file= pd.read_csv(DataPath+'CDL2GCAM_SRP_categories.csv', sep=',')
+# TODO: add path to price signals csv from config file
 
+profit_file=pd.read_csv(userPath+'IM3-BoiseState/ABM/PreprocessingTools/NewSyntheticOutput.csv')
 #---------------------------------------
 # 0. Declare Variables
 #---------------------------------------
@@ -64,28 +67,32 @@ domain = init.InitializeDomain(Ny, Nx)
 #---------------------------------------
 #  Initialize Crops
 #---------------------------------------
-Nc = 4 #there are actually 17 when the 1km is run, need random profit profiles for each of these 
-CropIDs =np.array([1,2,3,10]) # need to make this automatic depending on which crops show up (which of AllCropIDs == np.unique(lc))
-CropIDs= CropIDs.reshape((Nc,1))
-#CropIDs=np.arange(Nc).reshape((Nc,1)) + 1
+
+#ID crops based on those in the initial land cover
+
+# TODO: make sure that this key_file is being called to properly from config
+ag=np.where(key_file['SRB_cat'] == 'ag')
+CropIDs =np.int64(key_file['SRB_GCAM_id_list'][ag[0]])
+Num_crops = len(CropIDs)
+
 CropID_all = np.zeros((Nt,Ny,Nx))
 CropID_all[0,:,:] = lc #this will be added into the cell class
 
 #---------------------------------------
 #  Initialize Profits
 #---------------------------------------
-Profit_ant = np.zeros((Nt,Ny,Nx))
-Profit_act = np.zeros((Nt,Ny,Nx))
 
-Profit_ant[0,:,:] = 30000.0 + np.random.normal(loc=0.0,scale=1000.0,size=(1,Ny,Nx))
-#what is this 30,0000? what is the scale here?
-
-Profit_act[0,:,:] = Profit_ant[0,:,:]
-
-Profits = [] # A list of numpy arrays that will be Nt x Nc 
-Profits = cd.GeneratePrices(Nt)
-Profits = Profits[:, 0:Nc]
-#Profits = Profits[:, 2:6] The choice of crop profits will completely drive the outcome ... how do we use that?
+# initializes profits based on profit signals from csv output from generate synthetic prices 
+profits_actual = np.zeros((Nt,Ny,Nx))
+profit_signals=profit_file.iloc[:,2:].set_index(profit_file['CropID']) #subsets to just prices and sets index to numerical CropID
+for i in np.arange(Ny):
+    for j in np.arange(Nx):
+        CropInd= CropID_all[0,i,j]
+        if CropInd in (CropIDs):
+            profits_actual[0,i,j]= profit_signals.loc[CropInd][0]
+        else:
+            profits_actual[0,i,j]= 0
+            
 #---------------------------------------
 #  Initialize Agents
 #---------------------------------------
@@ -114,26 +121,18 @@ for i in np.arange(1,Nt):
         for k in np.arange(Nx):
             if domain[j,k].FarmerAgents:
                 #Assess Profit
-                Profit_last, Profit_pred = cd.AssessProfit(CropID_all[i-1,j,k], Profits[i-1,:], Profits[i,:], Nc, CropIDs)
+                profit_last, profit_pred = cd.AssessProfit(CropID_all[i-1,j,k], profits_actual[i-1,:], profit_signals[i,:], Num_crops, CropIDs)
                 #Decide on Crop
-                CropChoice, ProfitChoice = cd.DecideN(domain[j,k].FarmerAgents[0].alpha, domain[j,k].FarmerAgents[0].beta, fmin, fmax, n, Profit_last, CropIDs, \
-                                                          Profit_pred, rule=True)
+                CropChoice, profitChoice = cd.DecideN(domain[j,k].FarmerAgents[0].alpha, domain[j,k].FarmerAgents[0].beta, fmin, fmax, n, profit_last, CropIDs, profit_pred, rule=True)
                 
-                
-                CropID_all[i,j,k], Profit_ant[i,j,k], Profit_act[i,j,k] = cd.MakeChoice(CropID_all[i-1,j,k], Profit_last, CropChoice, ProfitChoice, seed=False) 
-                
-                CropChoice, ProfitChoice = cd.DecideN(domain[j,k].FarmerAgents[0].alpha, domain[j,k].FarmerAgents[0].beta, fmin, fmax, n, Profit_last, CropIDs, \
-                                                          Profit_pred, rule=True)
-                
-                CropID_all[i,j,k], Profit_ant[i,j,k], Profit_act[i,j,k] = cd.MakeChoice(CropID_all[i-1,j,k], Profit_last, CropChoice, ProfitChoice, seed = False) 
-                
+                CropID_all[i,j,k], profits_actual[i,j,k] = cd.MakeChoice(CropID_all[i-1,j,k], profit_last, CropChoice, profitChoice, seed=False) 
                 
                 #update agent attributes
                 domain[j,k].FarmerAgents[0].UpdateAge() #there needs to be a limit on this - e.g. what happens when farmers are over 90 - do they automatically get replaced with another?
  
 
 
-ppf.CropPerc(CropID_all, CropIDs, Nt, Nc) #save this to results output
+ppf.CropPerc(CropID_all, CropIDs, Nt, Num_crops) #save this to results output
 ppf.AgentAges(domain, AgentArray, Ny, Nx)
 
 #save 3D landcover coverage
