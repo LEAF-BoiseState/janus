1e2d7d000de47193cf5aaa1760cc9c7691cdb91d
crvernon <chris.vernon@pnnl.gov>
2019-10-14 17:34:00 -0400

    remove unused class vars

--- janus/tests/test_model_outputs.py
+++ janus/tests/test_model_outputs.py
@@ -10,8 +10,6 @@ class TestEqualOutputs(unittest.TestCase):
     """Test that the default outputs do not change."""
 
     DEFAULT_CONFIG_FILE = pkg_resources.resource_filename('janus', 'tests/data/config.yml')
-    COMP_OUTPUTS_DIR = pkg_resources.resource_filename('janus', 'tests/data/comp_outputs')
-    DEFAULT_OUTPUTS_DIR = pkg_resources.resource_filename('janus', 'tests/data/outputs')
 
     COMP_OUTPUT_DOMAIN = pkg_resources.resource_filename('janus', 'tests/data/comp_outputs/domain_3000m_20yr.npy')
     COMP_OUTPUT_LANDCOVER = pkg_resources.resource_filename('janus', 'tests/data/comp_outputs/landcover_3000m_20yr.npy')
@@ -22,7 +20,7 @@ class TestEqualOutputs(unittest.TestCase):
         # TODO: verify that this test will pass with current data
 
         # load comp data
-        # comp_domain_arr = np.load(TestEqualOutputs.COMP_OUTPUT_DOMAIN)
+        comp_domain_arr = np.load(TestEqualOutputs.COMP_OUTPUT_DOMAIN)
         comp_landcover_arr = np.load(TestEqualOutputs.COMP_OUTPUT_LANDCOVER)
         comp_profits_arr = np.load(TestEqualOutputs.COMP_OUTPUT_PROFITS)
 
@@ -30,7 +28,7 @@ class TestEqualOutputs(unittest.TestCase):
         res = Janus(config_file=TestEqualOutputs.DEFAULT_CONFIG_FILE, save_result=False, plot_results=False)
 
         # test that new outputs equal old outputs
-        # np.testing.assert_array_equal(res.agent_domain, comp_domain_arr)
+        np.testing.assert_array_equal(res.agent_domain, comp_domain_arr)
         np.testing.assert_array_equal(res.crop_id_all, comp_landcover_arr)
         np.testing.assert_array_equal(res.profits_actual, comp_profits_arr)
 
