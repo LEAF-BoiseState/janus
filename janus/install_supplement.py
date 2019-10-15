0ee4252623df960e5a44fe7d9098e65d6004962d
crvernon <chris.vernon@pnnl.gov>
2019-10-15 10:22:01 -0400

    change install protocol

--- janus/install_supplement.py
+++ janus/install_supplement.py
@@ -9,25 +9,13 @@ License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
 """
 
 import argparse
-import os
-import requests
-import sys
-import zipfile
+import pkg_resources
 
-from pkg_resources import get_distribution
-
-# get Python major version number
-pyversion = sys.version_info.major
-
-if pyversion <= 2:
-    from StringIO import StringIO as BytesIO
-else:
-    from io import BytesIO as BytesIO
+from distutils.dir_util import copy_tree
 
 
 class InstallSupplement:
-    """Download and unpack example data supplement from Zenodo that matches the current installed
-    distribution.
+    """Transfer tests data to a directory of the users choosing to execute model.
 
     :param example_data_directory:              Full path to the directory you wish to install
                                                 the example data to.  Must be write-enabled
@@ -35,41 +23,14 @@ class InstallSupplement:
 
     """
 
-    # URL for DOI minted example data hosted on Zenodo
-    # TODO:  replace link with actual data once it has been archived
-    DATA_VERSION_URLS = {'0.0.1': 'https://zenodo.org/record/2578287/files/example.zip?download=1'}
+    DATA_DIR = pkg_resources.resource_filename('janus', 'tests/data')
 
     def __init__(self, example_data_directory):
 
         # full path to the root directory where the example dir will be stored
         self.example_data_directory = example_data_directory
 
-        self.fetch_zenodo()
-
-    def fetch_zenodo(self):
-        """Download and unpack the Zenodo example data supplement for the
-        current distribution."""
-
-        # get the current version that is installed
-        current_version = get_distribution('janus').version
-
-        try:
-            data_link = InstallSupplement.DATA_VERSION_URLS[current_version]
-
-        except KeyError:
-            msg = "Link to data missing for current version:  {}.  Please contact admin."
-            raise(msg.format(current_version))
-
-        # retrieve content from URL
-        print("Downloading example data for version {}".format(current_version))
-        r = requests.get(data_link)
-
-        with zipfile.ZipFile(BytesIO(r.content)) as zipped:
-
-            # extract each file in the zipped dir to the project
-            for f in zipped.namelist():
-                print("Unzipped: {}".format(os.path.join(self.example_data_directory, f)))
-                zipped.extract(f, self.example_data_directory)
+        copy_tree(InstallSupplement.DATA_DIR, self.example_data_directory)
 
 
 if __name__ == "__main__":
