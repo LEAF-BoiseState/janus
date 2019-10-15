4d08cf3d36b8056dec0aca5f87e45d53e0db59a8
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-10-15 10:52:19 -0600

    Fixed the legend and saving of histogram

--- ABM/PostProcessing/FigureFuncs.py
+++ ABM/PostProcessing/FigureFuncs.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import os
 
 def CreateAnimation(CropID_all, Nt):
     ims = []
@@ -43,42 +44,63 @@ def dCdT(CropID_all, Nt):
    
 #stackplot of crops over time
 #automate stackplot naming conventions
-def CropPerc(CropID_all, CropIDs, Nt, Nc, scale, ResultsPath, key_file, ag_cats):
-    agTot=59 #need to automate what the total area in crops is - this will be a unit test when urban isnt changing
-    #np.any(CropID_all == CropIDs)
+def CropPerc(crop_id_all, CropIDs, nt, nc, scale, results_path, key_file, ag_cats):
+    ag_area=np.empty(shape=(nc, nt))
+    for t in np.arange(nt):
+       cur_crop = crop_id_all[t,:,:]
+       for c in np.arange(nc):
+           bools=(cur_crop == CropIDs[c])
+           ag_area[c,t]=np.sum(bools)
+        
+    agTot = np.sum(ag_area, axis=0)
+
     names = []
-    percentages=np.zeros((Nc, Nt))
-    for c in np.arange(Nc):
-        name='percentages['+str(c)+',:]'
+    percentages = np.zeros((nc, nt))
+    data = []
+    for c in np.arange(nc):
+        name = 'percentages[' + str(c) + ',:]'
         names.append(name)
-        for t in np.arange(Nt):
-            CropIx=CropIDs[c]
-            percentages[c,t]=np.sum((CropID_all[t,:,:] == CropIx))/agTot*100.0
-            
-    t = np.arange(Nt)
+        for t in np.arange(nt):
+            CropIx = CropIDs[c]
+            percentages[c, t] = np.sum((crop_id_all[t, :, :] == CropIx)) / agTot[t] * 100.0
+        data.append(percentages[c, :])
+    
+    y = np.vstack(data)
+    
+    t = np.arange(nt)
     plt.rcParams.update({'font.size': 16})
-    fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(12,12))
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 12))
     
-    #set colors to come color scheme w Nc colors
-    #figure out how to automate the number of percentages in the stackplot
-    ax.stackplot(t,percentages[0,:], percentages[1,:], percentages[2,:], percentages[3,:], labels=key_file['GCAM_SRB_Name'][ag_cats[0]]) 
-    ax.set_xlim([0,Nt-1])
-    ax.set_ylim([0,100])
+    active_crops=np.any(percentages, axis=1)
+    ag3=np.transpose(np.array(ag_cats))
+    ac=np.array(ag3[active_crops]).flatten()
+    labs=key_file['GCAM_SRB_Name'][ac]
+    
+    
+    ax.stackplot(t,y, labels=labs) 
+    ax.set_xlim([0, nt - 1])
+    ax.set_ylim([0, 100])
     ax.grid()
-    ax.legend(loc='lower left')
-    
-    ax.set_ylabel('Percent Crop Choice')  
+    ax.legend(loc='lower right')
+
     ax.set_ylabel('Percent Crop Choice')
-    ax.set_xlabel('Time [yr]')  
-  
-    plt.savefig(ResultsPath+'CropPercentages_'+str(scale)+'m_'+str(Nt)+'yr'.png,dpi=300,facecolor='w', edgecolor='w', 
-                bbox_inches='tight')
+    ax.set_xlabel('Time [yr]')
+
+    output_figure = os.path.join(results_path, 'CropPercentages_{}m_{}yr.png'.format(scale, nt))
+
+    plt.savefig(output_figure, dpi=300, facecolor='w', edgecolor='w', bbox_inches='tight')
+    plt.close(fig=None)
+
               
-def AgentAges(domain, AgentArray, Ny, Nx):
+def AgentAges(domain, AgentArray, Ny, Nx, nt, nc, scale, results_path):
     FarmerAges=[]
     for i in np.arange(Ny):
         for j in np.arange(Nx):
             if(AgentArray[i,j]=='aFarmer'):  
                 FarmerAges=np.append(FarmerAges, domain[i,j].FarmerAgents[0].Age)
                 
+    plt.rcParams.update({'font.size': 16})
     plt.hist(FarmerAges)
+    
+    output_figure = os.path.join(results_path, 'AgentAges_{}m_{}yr.png'.format(scale, nt))
+    plt.savefig(output_figure, dpi=300, facecolor='w', edgecolor='w', bbox_inches='tight')
