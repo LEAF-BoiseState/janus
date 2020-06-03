c0b13c86f69a1b3a6ef105a250212644e76d3411
crvernon <chrisrvernon@gmail.com>
2020-06-03 16:48:01 -0400

    remove call in __init__

--- janus/__init__.py
+++ janus/__init__.py
@@ -3,15 +3,3 @@ from janus.install_supplement import *
 from janus.preprocessing.get_gis_data import get_gis_data
 
 __all__ = ['Janus', 'InstallSupplement', 'get_gis_data']
-
-
-
-import janus
-
-# prepare GIS data
-janus.get_gis_data()
-
-# run model
-...
-
-
