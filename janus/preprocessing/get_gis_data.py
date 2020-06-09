b961c5cefb91c927a26a7a1c7e0edcf2c87e02e2
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-06-09 14:23:54 -0600

    Updated functions to reflect move into landcover_preprocessing.py and new inputs for paths

--- janus/preprocessing/get_gis_data.py
+++ janus/preprocessing/get_gis_data.py
@@ -3,7 +3,7 @@ Created on Mon Apr  8 21:55:00 2019
 
 @author: kek25
 
-Pre-process GIS data based on counties, base year, and resolution
+Select GIS data based on base year, and resolution and clip to counties
 """
 
 import os
@@ -14,7 +14,7 @@ import janus.preprocessing.geofxns as gf
 import janus.preprocessing.landcover_preprocessing as lc
 
 
-def get_gis_data(counties_shp, categories_csv, county_list, scale, year, gcam_data_directory,
+def get_gis_data(counties_shp, categories_csv, county_list, scale, year, raw_lc_dir, processed_lc_dir, init_lc_dir,
                  gcam_category_type='local_GCAM_id'):
     """Preprocess GIS data based on counties, base year, and resolution.
 
@@ -22,14 +22,11 @@ def get_gis_data(counties_shp, categories_csv, county_list, scale, year, gcam_da
     :type counties_shp:                     str
 
     :param categories_csv:                  Full path with file name and extension to the input categories CSV file
-                                            that bins CDL landclasses to GCAM landclasses
+                                            that bins CDL land classes to GCAM land classes
     :type categories_csv:                   str
 
     :param county_list:                     List of county names to process
     :type county_list:                      list
-
-    :param gcam_data_directory:             Full path to the directory containing the GCAM output raster
-    :param gcam_data_directory:             str
 
     :param scale:                           resolution of grid cells in meters
     :type scale:                            int
@@ -37,17 +34,20 @@ def get_gis_data(counties_shp, categories_csv, county_list, scale, year, gcam_da
     :param year:                            Four digit year to process (e.g., 2000)
     :type year:                             int
 
+    :param processed_lc_dir:                Full path to the directory containing the processed land cover data
+    :type processed_lc_dir:                 str
+
+    :param init_lc_dir:                   Full path to the directory where land cover initialization files are stored
+    :type init_lc_dir:                   str
+
     :param gcam_category_type:              Convert CDL data to GCAM categories of choice, Default 'local_GCAM_id' which
                                             is a set of ids that are specific to a local set of crop categories; where,
                                             'GCAM_id_list' is the standard set of GCAM global categories.
     :type gcam_category_type:               str
 
+    :param raw_lc_dir:
+
     """
-
-    # output directory
-    # TODO:  change `os.path.dirname(gcam_data_directory)` to an output_path parameter that is passed into this fn.
-    output_directory = os.path.dirname(gcam_data_directory)
-
     # read counties shapefile as geopandas data frame
     gdf_counties = gp.read_file(counties_shp)
     gdf_counties.set_index('county', inplace=True)
@@ -56,18 +56,17 @@ def get_gis_data(counties_shp, categories_csv, county_list, scale, year, gcam_da
     gdf_key = gp.read_file(categories_csv)
 
     # convert cdl data to GCAM categories of choice
-    lc.c2g(gdf_key, gcam_category_type)
+    lc.c2g(gdf_key, gcam_category_type, processed_lc_dir, raw_lc_dir)
 
     # convert GCAM file to scale of interest
-    lc.aggGCAM(scale, gcam_data_directory)
+    lc.aggGCAM(scale, processed_lc_dir)
 
-    # use the above file to create a polygon coverage
-    lc.grid2poly(year, scale, gcam_data_directory, output_directory)
+    # use the above file to create a polygon coverage & save; this allows for mapping each cell over time (?)
+    lc.grid2poly(year, scale, processed_lc_dir, init_lc_dir)
 
-    # use the poly grid to create the extent for the model
-    extent = gf.get_extent(counties_shp, county_list, scale, output_directory)
+    # use the poly grid to create the extent for the model - only needed if using other land cover data
+    lc.get_extent(counties_shp, county_list, scale, init_lc_dir)
 
-    # select initial gcam data from inital year
-    lc_raster = gf.get_gcam(counties_shp, county_list, gcam_data_directory)
-
-    return extent, lc_raster
+    # crop land cover data from initialization year
+    gcam_file = os.path.join(processed_lc_dir, 'gcam_'+str(int(year))+'_domain_'+str(int(scale))+'.tiff')
+    lc.get_gcam(counties_shp, county_list, gcam_file, init_lc_dir)
