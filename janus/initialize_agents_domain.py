a39b9c9f1ac340932199ed71d603bcba970d07fc
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-07-01 11:20:49 -0600

    Updating doc strings and function descriptions

--- janus/initialize_agents_domain.py
+++ janus/initialize_agents_domain.py
@@ -11,137 +11,184 @@ import janus.agents.urban as urban
 import janus.preprocessing.get_nass_agent_data as getNASS
 
 
-def initialize_domain(Ny, Nx):
+def initialize_domain(ny, nx):
+    """ Create empty domain array
+
+    :param ny:  Number of columns in domain
+    :type ny:   Int
+
+    :param nx:  Number of rows in domain
+    :type nx:   Int
+
+    :return:    Empty numpy array filled with class Dcell at each pixel
+    :type:      Numpy Array
     """
+    domain = np.empty((ny, nx), dtype=object)
 
-    :param Ny: Number of columns in domain
-    :param Nx: Number of rows in domain
+    for i in np.arange(ny):
 
-    :return: empty numpy array filled with class Dcell at each pixel
-
-    """
-    domain = np.empty((Ny, Nx), dtype=object)
-
-    for i in np.arange(Ny):
-
-        for j in np.arange(Nx):
+        for j in np.arange(nx):
 
             domain[i][j] = cell.Dcell()
 
     return domain
 
 
-def place_agents(Ny, Nx, lc, key_file, cat_option):
-    """ Place agents on the landscape based on landcover and associated categorization
+def place_agents(ny, nx, lc, key_file, cat_option):
+    """ Place agents on the landscape based on land cover and associated categorization
 
-    :param Ny: Number of columns in domain
-    :param Nx: Number of rows in domain
-    :param lc: Initial landcover numpy array
-    :param key_file: csv file with categorization from CDL categories to GCAM or user defined categories, see README file.
-    :param cat_option:  Set whether using 'GCAM' or 'local'categorization. If the local caracterization has been changed to 
-    have more or less categories, the number of rows to use in line 52/53 will need to be edited
+    :param ny:          Number of columns in domain
+    :type ny:           Int
+    
+    :param nx:          Number of rows in domain
+    :type ny:           Int
+    
+    :param lc:          Initial land cover numpy array
+    :type lc:           Numpy Array
+    
+    :param key_file:    csv file with categorization from CDL categories to GCAM or user defined categories, see README file.
+    :type key_file:     CSV file
+    
+    :param cat_option:  Set whether using 'GCAM' or 'local'categorization. If the local characterization has been changed to 
+                        have more or less categories, the number of rows to use in line 52/53 will need to be edited
+    :type cat_option:   String
 
-    :return: numpy array of strings with each agent type
+    :return:            numpy array of strings with each agent type
+    :type:              Numpy Array
 
     """
-    AgentArray = np.empty((Ny, Nx), dtype='U10')
+    agent_array = np.empty((ny, nx), dtype='U10')
 
     if cat_option == 'local':
         
-        agent_Cat = key_file['local_cat'][0:28]
+        agent_cat = key_file['local_cat'][0:28]
         code = key_file['local_GCAM_id_list'][0:28]
 
     elif cat_option == 'GCAM':
 
-        agent_Cat = key_file['GCAM_cat'][0:24]
+        agent_cat = key_file['GCAM_cat'][0:24]
         code = key_file['GCAM_id_list'][0:24]
 
-    ag = np.array(code[agent_Cat == 'ag']).astype(int)
-    urb = np.array(code[agent_Cat == 'urb']).astype(int)
-    water = np.array(code[agent_Cat == 'water']).astype(int)
-    empty = np.array(code[agent_Cat == 'nat']).astype(int)
+    ag = np.array(code[agent_cat == 'ag']).astype(int)
+    urb = np.array(code[agent_cat == 'urb']).astype(int)
+    water = np.array(code[agent_cat == 'water']).astype(int)
+    empty = np.array(code[agent_cat == 'nat']).astype(int)
 
     # this works, would be better without the for loops
     for i in ag:
-        AgentArray[lc[0] == i] = farmer.Farmer.__name__
+        agent_array[lc[0] == i] = farmer.Farmer.__name__
 
     for i in water:
-        AgentArray[lc[0] == i] = 'water'
+        agent_array[lc[0] == i] = 'water'
 
     for i in urb:
-        AgentArray[lc[0] == i] = urban.Urban.__name__
+        agent_array[lc[0] == i] = urban.Urban.__name__
 
     for i in empty:
-        AgentArray[lc[0] == i] = 'empty'
+        agent_array[lc[0] == i] = 'empty'
 
-    return AgentArray
+    return agent_array
 
 
-def agents(AgentArray, domain, dist2city, TenureCDF, AgeCDF, switch, Ny, Nx, lc, p, attr):
+def agents(agent_array, domain, dist2city, tenure_cdf, age_cdf, switch, ny, nx, lc, p, attr):
     """Place agent structures onto landscape and define attributes.
 
-    :param AgentArray: Numpy array of strings of location of each agent type
-    :param domain:     Initial domain
-    :param dist2city:  Numpy array of distance to city
-    :param TenureCDF:  CDF of tenure type in the domain
-    :param AgeCDF:     CDF of ages in the domain
-    :param switch:     List of lists of parameter sets to describe agent switching behavior
-    :param Ny:         Number of columns in domain
-    :param Nx:         Number of rows in domain
-    :param lc:         Initial land cover numpy array
-    :param p:          Percentage of switching averse farming agents
-    :param attr:       A boolean indicating whether or not to use switching curves based on tenure and age attributes
+    :param agent_array: Numpy array of strings defining each agent type
+    :type agent_array:  Numpy Array
 
-    :return:           Domain with agents in each dCell
+    :param domain:      Initial domain filled with class Dcell
+    :type domain:       Numpy Array
+
+    :param dist2city:   Numpy array of distance to city (float)
+    :type dist2city:    Numpy Array
+
+    :param tenure_cdf:  CDF of tenure type in the domain
+    :type tenure_cdf:
+
+    :param age_cdf:     CDF of ages in the domain
+    :type age_cdf:
+
+    :param switch:      List of lists of parameter sets to describe agent switching behavior
+    :type switch:       List
+
+    :param ny:          Number of columns in domain
+    :type ny:           Int
+
+    :param nx:          Number of rows in domain
+    :type nx:           Int
+
+    :param lc:          Initial land cover numpy array
+    :type lc:           Numpy Array
+
+    :param p:           Percentage of switching averse farming agents
+    :type p:            Float
+
+    :param attr:        A boolean indicating whether or not to use switching curves based on tenure and age attributes
+    :type attr:         Bool
+
+    :return:            Domain with agents in each dCell
+    :type:
 
     """
-    for i in np.arange(Ny):
+    for i in np.arange(ny):
 
-        for j in np.arange(Nx):
+        for j in np.arange(nx):
 
-            if AgentArray[i][j] == farmer.Farmer.__name__:
+            if agent_array[i][j] == farmer.Farmer.__name__:
 
-                AgentData = getNASS.farmer_data(TenureCDF, AgeCDF, switch, dist2city[i][j], p, attr)
-                NewAgent = farmer.Farmer(Age=AgentData["AgeInit"], LandStatus=AgentData["LandStatus"],
-                                          Dist2city=AgentData["Dist2city"], nFields=AgentData['nFields'],
-                                          alpha=AgentData['Alpha'],
-                                          beta=AgentData['Beta'])  # this is passing actual agent data
-                domain[i][j].add_agent(NewAgent)
+                agent_data = getNASS.farmer_data(tenure_cdf, age_cdf, switch, dist2city[i][j], p, attr)
+                new_agent = farmer.Farmer(Age=agent_data["AgeInit"], LandStatus=agent_data["LandStatus"],
+                                          Dist2city=agent_data["Dist2city"], nFields=agent_data['nFields'],
+                                          alpha=agent_data['Alpha'],
+                                          beta=agent_data['Beta'])  # this is passing actual agent data
+                domain[i][j].add_agent(new_agent)
 
-            if AgentArray[i][j] == urban.Urban.__name__:
+            if agent_array[i][j] == urban.Urban.__name__:
 
-                AgentData = getNASS.urban_data(lc[0][i][j])
-                NewAgent = urban.Urban(density=AgentData["Density"])
-                domain[i][j].add_agent(NewAgent)
+                agent_data = getNASS.urban_data(lc[0][i][j])
+                new_agent = urban.Urban(density=agent_data["Density"])
+                domain[i][j].add_agent(new_agent)
 
     return domain
 
 
-def profits(profit_signals, Nt, Ny, Nx, CropID_all, CropIDs):
+def init_profits(profit_signals, nt, ny, nx, crop_id_all, crop_ids):
     """Initialize np array of profits
 
-    :param profit_signals: data frame of profit signals created from generate synthetic prices, or user supplied
-    :param Nt: Number of timesteps
-    :param Ny: Number of columns in domain
-    :param Nx: Number of rows in domain
-    :param CropID_all: Nt x Nx x Ny np array of current land cover
-    :param CropIDs: Num_crop x 1 np array of crop ids
+    :param profit_signals:  Profit signals created from generate synthetic prices, or user supplied
+    :type profit_signals:   Numpy  Array
 
-    :return: np array of initial profits based on price signals
+    :param nt:              Number of time steps
+    :type nt:               Int
+
+    :param ny:              Number of columns in domain
+    :type ny:               Int
+
+    :param nx:              Number of rows in domain
+    :type  nx:              Int
+
+    :param crop_id_all:     nt x nx x ny np array of current land cover
+    :type crop_id_all:      Numpy Array
+
+    :param crop_ids:        Num_crop x 1 np array of crop ids
+    :type crop_ids:         Numpy Array
+
+    :return:                Initial profits based on price signals
+    :type:                  Numpy Array
 
     """
 
-    profits_actual = np.zeros((Nt, Ny, Nx))
+    profits_actual = np.zeros((nt, ny, nx))
 
-    for i in np.arange(Ny):
+    for i in np.arange(ny):
 
-        for j in np.arange(Nx):
+        for j in np.arange(nx):
 
-            CropInd = CropID_all[0, i, j]
-            CropIx = np.where(CropIDs == CropInd)
+            crop_ind = crop_id_all[0, i, j]
+            crop_ix = np.where(crop_ids == crop_ind)
 
-            if CropInd in (CropIDs):
-                profits_actual[0, i, j] = profit_signals[CropIx[0][0], 0]
+            if crop_ind in crop_ids:
+                profits_actual[0, i, j] = profit_signals[crop_ix[0][0], 0]
 
             else:
                 profits_actual[0, i, j] = 0
