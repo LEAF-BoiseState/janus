6a793dbd0fa2f340383b80bc0765ec98b2959a03
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-07-01 13:25:23 -0600

    Turned into regular function, updated doc strings

--- janus/preprocessing/convert_gcam_usa_prices.py
+++ janus/preprocessing/convert_gcam_usa_prices.py
@@ -14,47 +14,29 @@ import pandas as pd
 from scipy.stats import linregress
 
 
-def main(argv):
-    """Description
+def gcam_usa_price_converter(gcam_profits, profits_out, key_file, nc, nt, year):
+    """Convert GCAM USA prices to the crop categories defined in the key file and format to be used by Janus
 
-    :param argv: Array of 5 command line arguments passed from the __main__ function
-    :param argv[0]: Name of this function (convert_GCAM-USA_prices)
-    :param argv[1]: Number of crops to create profit time series for
-    :param argv[2]: Number of time steps in the time series
-    :param argv[3]: Name of CSV file with GCAM-USA outputs including path if CSV file is in a different directory
-    :param argv[4]: Name of CSV file to which profit time series will be written, including path to output if in
-    a different directory than the script
-    :param argv[5]: Start year of model run
-    :param argv[6]: Name of key file including path if not in directory
+    :param gcam_profits:    Full path and file name to GCAM-USA outputs
+    :type gcam_profits:     String
+    :param profits_out:     Full path and output file name to CSV file to which profit time series will be written
+    :type profits_out:      String
+    :param key_file:        Full path and file name of key file
+    :type key_file:         String
+    :param nc:              Number of crops to create profit time series for
+    :type nc:               Integer
+    :param nt:              Number of time steps in the time series
+    :type nt:               Integer
+    :param year:            Start year of model run
+    :type year:             Integer
 
-    :return: null (output written to file)
+    :return:                null (output written to file)
     """
 
-    if len(argv) != 7:
-        print('\nERROR: Incorrect number of command line arguments\n')
-        print(
-            'Usage: convert_GCAM-USA_prices.py <no. crops> <no. time steps> <Input CSV file> <Output CSV file> <Key file> \n')
-        print('\tconvert_gcamland_prices.py   = Name of this python script')
-        print('\t<no. crops>                  = Number of crops to synthesize prices for')
-        print('\t<no. time steps>             = Number of time steps to generate prices for')
-        print('\t<CSV file>                   = CSV file containing crop information')
-        print('\t                               (see documentation)')
-        print('\t<Output CSV file>            = CSV file in which to save output prices')
-        print('\t<start year                  = Year that Janus is initiated')
-        print('\t<Key file>                   = Key file that contains conversions between GCAM and janus\n')
-        sys.exit()
-
-    nc = int(argv[1])
-    nt = int(argv[2])
-    CropFileIn = argv[3]
-    CropFileOut = argv[4]
-    year = int(argv[5])
-    key_file = argv[6]
-
     # Error traps
-    assert nc > 0, 'convert_GCAM-USA_prices.py ERROR: Negative number of crops encountered'
-    assert nt > 0, 'convert_GCAM-USA_prices.py ERROR: Negative number of time steps encountered'
-    assert nc <= 28, 'convert_GCAM-USA_prices.py ERROR: Too many crops encountered'
+    assert nc > 0, 'convert_gcam_usa_prices.py ERROR: Negative number of crops encountered'
+    assert nt > 0, 'convert_gcam_usa_prices.py ERROR: Negative number of time steps encountered'
+    assert nc <= 28, 'convert_gcam_usa_prices.py ERROR: Too many crops encountered'
 
     # function to find nearest value
     def find_nearest(array, value):
@@ -63,7 +45,7 @@ def main(argv):
         return array[idx]
 
     # read input data
-    gcam_dat = pd.read_csv(CropFileIn)
+    gcam_dat = pd.read_csv(gcam_profits)
     key = pd.read_csv(key_file)
 
     # parse input data
@@ -74,8 +56,8 @@ def main(argv):
     srb_ids = key['local_GCAM_id_list'][valid_crops[0]]
 
     # TODO fix this assert, need to drop crops that aren't in the SRB
-    #assert all(np.sort(gcam_usa_names.unique()) == np.sort(crop_names)), 'convert_GCAM-USA_prices.py ERROR: Crop ' \
-                                                                         #'names from GCAM-USA do not match keyfile'
+    #assert all(np.sort(gcam_usa_names.unique()) == np.sort(crop_names)), 'convert_gcam_usa_prices.py ERROR: Crop ' \
+                                                                         #'names from gcam_usa do not match keyfile'
 
     # find start and end years from gcam data
     int_col = np.where(gcam_dat.columns == str(year))[0][0]
@@ -116,10 +98,6 @@ def main(argv):
         sys.exit()
 
     # TODO: fix the warning here
-    with open(CropFileOut, 'w') as fp:
+    with open(profits_out, 'w') as fp:
         np.savetxt(fp, out, delimiter=',', fmt='%.5f')
         fp.close()
-
-
-if __name__ == "__main__":
-    main(sys.argv)
