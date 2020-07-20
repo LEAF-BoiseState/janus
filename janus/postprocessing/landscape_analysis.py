9a9a327f463ee8307949b0e00c141e971cefbd3e
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-07-20 15:35:52 -0600

    Added notes and addnl metric

--- janus/postprocessing/landscape_analysis.py
+++ janus/postprocessing/landscape_analysis.py
@@ -21,6 +21,8 @@ def lc_analysis(lc, scale):
     """
 
     ls = pls.Landscape(lc)
+    # TODO: evaluate which subset of metrics are of most importance to cut down on compute time
     patch_metrics_df = ls.compute_patch_metrics_df()
     crop_totals = patch_metrics_df['class_val'].value_counts()
     class_metrics_df = ls.compute_class_metrics_df()
+    landscape_metrics_df = ls.compute_landscape_metrics_df()
