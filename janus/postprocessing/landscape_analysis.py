eac29adea1ba48f0d94c36ebe886341e15311e7e
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-07-20 12:55:57 -0600

    initial development of landscape analysis function

---
+++ janus/postprocessing/landscape_analysis.py
@@ -0,0 +1,26 @@
+"""
+Created on Tue July 20 2020
+
+@author: kendrakaiser
+
+Suite of functions to evaluate heterogeneity of landscape
+"""
+
+import pylandstats as pls
+
+
+def lc_analysis(lc, scale):
+    """
+    :param lc:      stack of land cover data
+    :type lc:       numpy
+
+    :param scale:   resolution of grid
+    :type scale:    int
+
+    :return:        figures and csv of analysis
+    """
+
+    ls = pls.Landscape(lc)
+    patch_metrics_df = ls.compute_patch_metrics_df()
+    crop_totals = patch_metrics_df['class_val'].value_counts()
+    class_metrics_df = ls.compute_class_metrics_df()
