e0116e01b0e52c8d0e63565f108241b22a60e44c
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-11-12 14:14:06 -0700

    First attempt at a script to run ensembles

---
+++ janus/ensemble_runs.py
@@ -0,0 +1,32 @@
+import sys
+
+import janus
+
+
+def run_example(f):
+    """Sample function to run example.
+
+    :param f:           Full path with file name and extension to a config.yml file
+
+    :return:            Model executes, returns Janus class attributes
+
+    """
+    return janus.Janus(config_file=f)
+
+
+def myscript(config_file, iteration_number):
+    xfile_name = "x%d.txt" % iteration_number
+    yfile_name = "y%d.txt" % iteration_number
+    with open(xfile_name, "w") as xf:
+        with open(yfile_name, "w") as yf:
+            run_example(config_file)
+
+
+def main(unused_command_line_args):
+    for i in xrange(1000):
+        myscript('/Users/kek25/Desktop/Janus_run/profitmax/highswitch/config.yml', i)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
