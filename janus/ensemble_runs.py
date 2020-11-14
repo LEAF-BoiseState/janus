c74d5526718b3af12cf656dbc95e5cf3e2231747
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-11-13 17:48:14 -0700

    Removed naming stuff, added it to the main model

--- janus/ensemble_runs.py
+++ janus/ensemble_runs.py
@@ -13,15 +13,9 @@ def run_example(f):
     return janus.Janus(config_file=f)
 
 
-def myscript(config_file, iteration_number):
-    xfile_name = "x%d.txt" % iteration_number
-    with open(xfile_name, "w") as xf:
-        run_example(config_file)
-
-
 def main(unused_command_line_args):
     for i in range(3):
-        myscript('/Users/kek25/Desktop/Janus_run/profitmax/highswitch/config.yml', i)
+        run_example('/Users/kek25/Desktop/Janus_run/profitmax/highswitch/config.yml')
     return 0
 
 
