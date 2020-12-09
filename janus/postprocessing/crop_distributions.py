382f37567d2ebc4574c2ea3b463b8cb0cd5a7116
Kendra Kaiser <kendrakaiser@boisestate.edu>
2020-12-09 16:49:45 -0700

    Changed output figures

--- janus/postprocessing/crop_distributions.py
+++ janus/postprocessing/crop_distributions.py
@@ -7,25 +7,24 @@ import glob
 import numpy as np
 import pandas as pd
 import seaborn as sb
-dir = '/Users/kendrakaiser/Desktop/Janus_run/smallworld/lowswitch/*.csv'
-#dir = '/Users/kendrakaiser/Desktop/Janus_run/profitmax/'
-#dir = '/Users/kendrakaiser/Desktop/Janus_run/erdosrenyi/'
 
-files = glob.glob(dir)
+
+#dir_name = '/Users/kendrakaiser/Desktop/Janus_run/smallworld/highswitch/*.csv'
+#dir_name = '/Users/kendrakaiser/Desktop/Janus_run/profitmax/lowswitch/*.csv'
+dir_name = '/Users/kendrakaiser/Desktop/Janus_run/erdosrenyi/highswitch_ed01/*.csv'
+
+files = glob.glob(dir_name)
 data = np.zeros((1, 20))
 for fname in files:
     with open(fname, 'r') as f:
         dat = pd.read_csv(f)
-        data = np.append(data, np.array(dat.iloc[:,-1]).reshape((1, len(dat))), axis=0)
+        data = np.append(data, np.array(dat.iloc[:, -1]).reshape((1, len(dat))), axis=0)
 
 data = data[1:, :]
-data1 = data[:, ~np.all(np.isnan(data), axis=0)]
-data = pd.DataFrame(np.nan_to_num(data))
+data1 = data[:, ~np.all(data == 0, axis=0)]
+# data = pd.DataFrame(np.nan_to_num(data))
 
-
-g = sb.FacetGrid(data, col='columns')
-g = (g.map(sb.distplot, 'value'))
-
-data.plot.kde()
-
-sb.displot(data1, kind='kde')
+p = sb.displot(data1, kind='kde')
+p = (p.set_axis_labels("Agents","Percent"))
+p.set(xlim=(0, 400))
+p.savefig('/Users/kendrakaiser/Desktop/Janus_run/ed01.eps')
